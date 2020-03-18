8fc25778aba2c49db3eebaf50e9befabc5994bd6
dval <nuclearr.d@gmail.com>
2020-03-18 17:02:36 -0400

    boundary batch run

---
+++ 7-classification/7.9-get-predictions-from-BERT.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# This line tells the shell how to execute this script, and is unrelated 
+# to SLURM.
+   
+# at the beginning of the script, lines beginning with "#SBATCH" are read by
+# SLURM and used to set queueing options. You can comment out a SBATCH 
+# directive with a second leading #, eg:
+##SBATCH --nodes=1
+   
+# we need 1 node, will launch a maximum of one task. The task uses 2 CPU cores  
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=1
+     
+# we expect the job to finish within 1 hours. If it takes longer than 1
+# hours, SLURM can kill it: 
+#SBATCH --time=24:00:00
+   
+# we expect the job to use no more than 10GB of memory:
+#SBATCH --mem=60GB
+#SBATCH --gres=gpu:1
+   
+# we want the job to be named "myMatlabTest" rather than something generated 
+# from the script name. This will affect the name of the job as reported
+# by squeue: 
+#SBATCH --job-name=predictions
+ 
+# when the job ends, send me an email at this email address.
+# replace with your email address, and uncomment that line if you really need to receive an email.
+#SBATCH --mail-type=END
+#SBATCH --mail-user=samuel.fraiberger@nyu.edu
+   
+# both standard output and standard error are directed to the same file.
+# It will be placed in the directory I submitted the job from and will
+# have a name like slurm_12345.out
+#SBATCH --output=slurm_%j.out
+ 
+# once the first non-comment, non-SBATCH-directive line is encountered, SLURM 
+# stops looking for SBATCH directives. The remainder of the script is  executed
+# as a normal Unix shell script
+  
+# first we ensure a clean running environment:
+module purge
+# and load the module for the software we are using:
+#module load matlab/2016b
+#module load mongodb/3.4.10
+module load anaconda3/2019.10
+source ~/mypython/py3.7/bin/activate
+
+# the script will have started running in $HOME, so we need to move into the 
+# directory we just created earlier
+cd /scratch/da2734/twitter/7-classification
+
+# now start the job:
+srun time python -u ./py/7.9-get-predictions-from-BERT.py > ./log/7.9-get-predictions-from-BERT-${SLURM_ARRAY_TASK_ID}-$(date +%s).log 2>&1
+#cat thtest.m | srun matlab -nodisplay
+# Leave a few empty lines in the end to avoid occasional EOF trouble.
+
+exit
---
+++ 7-classification/plot_json.py
@@ -0,0 +1,63 @@
+#gets all this setup
+import time
+start_time = time.time()
+
+import pandas as pd
+import random
+import numpy as np
+
+results = pd.read_json('./mturk_mar6/results.json')
+
+results = results.T
+
+results.index = results['epoch']
+print(results.head())
+
+
+results = results[:20]
+
+print('plotting...')
+
+# # from plotnine import ggplot, geom_point, aes, stat_smooth
+# from plotnine import ggplot, geom_point, aes, stat_smooth
+# # from plotnine.data import mtcars
+#
+# my_plot = (ggplot(results)
+#  + geom_point(aes('epoch', 'train_loss'), size = .1, alpha = 0.3, color='red')
+#  + geom_point(aes('epoch', 'loss'), size = .1, alpha = 0.3, color='red')
+#  # + stat_smooth(size = .8, alpha = 0.5, se = False, method = 'loess')
+#            )
+
+ # + facet_wrap('~gear'))
+
+# print(p)
+# my_plot.save("results.png", width=5, height=5, dpi=300)
+
+
+# import numpy as np
+import matplotlib.pyplot as plt
+
+# # Create some mock data
+# t = np.arange(0.01, 10.0, 0.01)
+# data1 = np.exp(t)
+# data2 = np.sin(2 * np.pi * t)
+
+fig, ax1 = plt.subplots()
+
+color = 'tab:red'
+ax1.set_xlabel('epoch')
+ax1.set_ylabel('train_loss', color=color)
+ax1.plot(results['epoch'], results['train_loss'], color=color)
+ax1.tick_params(axis='y', labelcolor=color)
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color = 'tab:blue'
+ax2.set_ylabel('val loss', color=color)  # we already handled the x-label with ax1
+ax2.plot(results['epoch'], results['loss'], color=color)
+ax2.tick_params(axis='y', labelcolor=color)
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+# plt.show()
+
+plt.savefig('results_20.png', dpi=100)
