d3af8157ef924f44d4d36ffbbfb0e240b320326f
Manuel <manuel.tonneau@mailbox.org>
2020-05-18 10:10:10 +0200

    Update name of output folder

--- code/8-training_binary/training_binary.sh
+++ code/8-training_binary/training_binary.sh
@@ -4,14 +4,11 @@ DATA_FOLDER=$1
 MODEL_TYPE=$2
 MODEL_NAME=$3
 
-mkdir results
-cd results
-mkdir ${DATA_FOLDER}
-cd ..
+mkdir results_${MODEL_NAME}_${DATA_FOLDER}
 
 echo '***********************STARTING TRAINING ON LABEL lost_job_1mo***************************************************'
 python3 twitter/code/train_binary/train_binary.py --input_data_folder twitter/data/${DATA_FOLDER}/data_binary_pos_neg_balanced_removed_allzeros \
- --results_folder results/${DATA_FOLDER} \
+ --results_folder results_${MODEL_NAME}_${DATA_FOLDER} \
  --training_description bert_conversational_on_may5_data \
  --label lost_job_1mo \
  --model_type ${MODEL_TYPE} \
@@ -21,7 +18,7 @@ echo '***********************DONE TRAINING ON LABEL lost_job_1mo****************
 
 echo '***********************STARTING TRAINING ON LABEL is_unemployed**************************************************'
 python3 twitter/code/train_binary/train_binary.py --input_data_folder twitter/data/${DATA_FOLDER}/data_binary_pos_neg_balanced_removed_allzeros \
- --results_folder results/${DATA_FOLDER} \
+ --results_folder results_${MODEL_NAME}_${DATA_FOLDER} \
  --training_description bert_conversational_on_may5_data \
  --label is_unemployed \
  --model_type ${MODEL_TYPE} \
@@ -31,7 +28,7 @@ echo '***********************DONE TRAINING ON LABEL is_unemployed***************
 
 echo '***********************STARTING TRAINING ON LABEL job_search*****************************************************'
 python3 twitter/code/train_binary/train_binary.py --input_data_folder twitter/data/${DATA_FOLDER}/data_binary_pos_neg_balanced_removed_allzeros \
- --results_folder results/${DATA_FOLDER} \
+ --results_folder results_${MODEL_NAME}_${DATA_FOLDER} \
  --training_description bert_conversational_on_may5_data \
  --label job_search \
  --model_type ${MODEL_TYPE} \
@@ -41,7 +38,7 @@ echo '***********************DONE TRAINING ON LABEL job_search******************
 
 echo '***********************STARTING TRAINING ON LABEL is_hired_1mo***************************************************'
 python3 twitter/code/train_binary/train_binary.py --input_data_folder twitter/data/${DATA_FOLDER}/data_binary_pos_neg_balanced_removed_allzeros \
- --results_folder results/${DATA_FOLDER} \
+ --results_folder results_${MODEL_NAME}_${DATA_FOLDER} \
  --training_description bert_conversational_on_may5_data \
  --label is_hired_1mo \
  --model_type ${MODEL_TYPE} \
@@ -51,7 +48,7 @@ echo '***********************DONE TRAINING ON LABEL is_hired_1mo****************
 
 echo '***********************STARTING TRAINING ON LABEL job_offer******************************************************'
 python3 twitter/code/train_binary/train_binary.py --input_data_folder twitter/data/${DATA_FOLDER}/data_binary_pos_neg_balanced_removed_allzeros \
- --results_folder results/${DATA_FOLDER} \
+ --results_folder results_${MODEL_NAME}_${DATA_FOLDER} \
  --training_description bert_conversational_on_may5_data \
  --label job_offer \
  --model_type ${MODEL_TYPE} \
