458c70ca6db46d80449542f04890a247e39f719f
Manuel <manuel.tonneau@mailbox.org>
2020-05-21 16:52:35 +0200

    Update folder name

--- code/11-baseline/logit_glove/preprocessing.sh
+++ code/11-baseline/logit_glove/preprocessing.sh
@@ -3,7 +3,7 @@
 DATA_FOLDER=$1
 TWITTER_REPO_PATH=$2
 
-DATA_PATH=${TWITTER_REPO_PATH}/data/${DATA_FOLDER}/data_binary_pos_neg_balanced_removed_allzeros
+DATA_PATH=${TWITTER_REPO_PATH}/data/${DATA_FOLDER}/data_binary_pos_neg_balanced
 mkdir ${DATA_PATH}/preprocessed_glove
 
 ls ${DATA_PATH} -I "label_*" | xargs -n 1 -P 2 -I {} python preprocessing.py --input_file_path ${DATA_PATH}/{} --output_folder ${DATA_PATH}/preprocessed_glove
