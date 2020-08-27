897b72920896328b0f195016b520b009ef9a110b
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-08-27 15:34:54 +0200

    Add py file that selects tweets from inference output

---
+++ code/12-active_learning/active_learning.py
@@ -0,0 +1,39 @@
+
+# For one label, import CSV with all tweets from random and probabilities and select tweets to send to labelling
+
+import os
+import logging
+import argparse
+from simpletransformers.classification import ClassificationModel
+import pandas as pd
+from pandas.api.types import is_string_dtype
+from pandas.api.types import is_numeric_dtype
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import sklearn
+from scipy.special import softmax
+import numpy as np
+from datetime import datetime
+import time
+import pytz
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def get_args_from_command_line():
+    """Parse the command line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inference_data_path", type=str, help="Path to the inference data. Must be in csv format.",
+                        default="")
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    # Define args from command line
+    args = get_args_from_command_line()
+    # Import inference data
+    inference_df = pd.read_csv(args.inference_data_path)
+    
