28071693c5d752f6211100174929f1cdfca0dd20
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-09-02 10:25:04 +0200

    Delete useless imports

--- code/2-twitter_labor/5-active_learning/process_inference_output.py
+++ code/2-twitter_labor/5-active_learning/process_inference_output.py
@@ -1,20 +1,9 @@
 import os
 import logging
 import argparse
-from simpletransformers.classification import ClassificationModel
 import pandas as pd
-from pandas.api.types import is_string_dtype
-from pandas.api.types import is_numeric_dtype
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-import sklearn
-from scipy.special import softmax
 import numpy as np
-from datetime import datetime
-import time
 import pytz
-from os import listdir
-from os.path import isfile, join
 import pyarrow
 from pathlib import Path
 
@@ -39,6 +28,7 @@ if __name__ == "__main__":
     random_data_dir = Path('/scratch/spf248/twitter/data/classification/US/random')
     full_random_df = pd.concat(pd.read_parquet(parquet_file, columns=['tweet_id', 'text']) for parquet_file in random_data_dir.glob('*.parquet'))
     full_random_df = full_random_df.set_index(['tweet_id'])
+    print("Loaded all random data")
     for column in ['is_hired_1mo', 'is_unemployed', 'job_offer', 'job_search', 'lost_job_1mo']:
         inference_data_dir = Path(args.inference_output_folder)
         full_inference_df = pd.concat(pd.read_parquet(parquet_file) for parquet_file in inference_data_dir.glob('*.parquet'))
