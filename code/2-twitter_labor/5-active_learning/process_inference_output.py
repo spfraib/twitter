ab7ea96b7ada19f27c83cedd6365ed910731253f
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-09-02 09:54:53 +0200

    Modify py file for merge

--- code/2-twitter_labor/5-active_learning/process_inference_output.py
+++ code/2-twitter_labor/5-active_learning/process_inference_output.py
@@ -13,6 +13,10 @@ import numpy as np
 from datetime import datetime
 import time
 import pytz
+from os import listdir
+from os.path import isfile, join
+import pyarrow
+from pathlib import Path
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
@@ -32,4 +36,14 @@ if __name__ == "__main__":
     # Define args from command line
     args = get_args_from_command_line()
     # Import inference data
-    inference_df = pd.read_csv(args.inference_data_path)
+    random_data_dir = Path('/scratch/spf248/twitter/data/classification/US/random')
+    full_random_df = pd.concat(pd.read_parquet(parquet_file, columns=['tweet_id', 'text']) for parquet_file in random_data_dir.glob('*.parquet'))
+    full_random_df = full_random_df.set_index(['tweet_id'])
+    for column in ['is_hired_1mo', 'is_unemployed', 'job_offer', 'job_search', 'lost_job_1mo']:
+        inference_data_dir = Path(args.inference_output_folder)
+        full_inference_df = pd.concat(pd.read_parquet(parquet_file) for parquet_file in inference_data_dir.glob('*.parquet'))
+        full_inference_with_text_df = full_inference_df.join(full_random_df)
+        print("Column: {}".format(column))
+        print(full_inference_with_text_df.head())
+
+
