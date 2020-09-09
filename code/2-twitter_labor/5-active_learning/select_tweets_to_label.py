81237d451ad3a0d5a6d2442cb775b7e0a6f71c4c
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-09-09 10:57:45 +0200

    Finalize keyword exploration with lift

--- code/2-twitter_labor/5-active_learning/select_tweets_to_label.py
+++ code/2-twitter_labor/5-active_learning/select_tweets_to_label.py
@@ -23,15 +23,15 @@ def get_args_from_command_line():
                         help="Path to the inference data. Must be in csv format.",
                         default="")
     parser.add_argument("--N_exploit", type=int, help="Number of tweets to label for exploitation.", default="")
-    parser.add_argument("--N_explore_kw", type=int, help="Number of tweets to label for keyword exploration.",
+    parser.add_argument("--N_explore_kw", type=int, help="Number of tweets to label for keyword explore.",
                         default="")
     parser.add_argument("--K_tw_exploit", type=int, help="Number of top tweets to study for exploitation.", default="")
-    parser.add_argument("--K_tw_explore_kw", type=int, help="Number of top tweets to study for keyword exploration.",
+    parser.add_argument("--K_tw_explore_kw", type=int, help="Number of top tweets to study for keyword explore.",
                         default="")
-    parser.add_argument("--K_tw_explore_sent", type=int, help="Number of top tweets to study for sentence exploration.",
+    parser.add_argument("--K_tw_explore_sent", type=int, help="Number of top tweets to study for sentence explore.",
                         default="")
     parser.add_argument("--K_kw_explore", type=int,
-                        help="Number of keywords to select for keyword exploration.", default="")
+                        help="Number of keywords to select for keyword explore.", default="")
 
     args = parser.parse_args()
     return args
@@ -52,7 +52,6 @@ def get_token_in_sequence_with_most_attention(model, tokenizer, input_sequence):
     print(attention_scores_dict)
     return {'token_index': max(attention_scores_dict, key=attention_scores_dict.get),
             'token_str': tokenized_input_sequence[max(attention_scores_dict, key=attention_scores_dict.get)]}
-
 
 
 def word_count(df, column):
@@ -94,25 +93,36 @@ if __name__ == "__main__":
         all_data_df = pd.read_parquet(input_parquet_path)
         all_data_df = all_data_df.sort_values(by=["score"], ascending=False).reset_index()
         all_data_df['tokenized_text'] = all_data_df['text'].apply(tokenizer.tokenize)
-        # exploit
+        # exploit (final data in exploit_data_df)
         exploit_data_df = all_data_df[:args.N_exploit]
-        # exploration (keyword lift)
-        exploration_kw_data_df = all_data_df[:args.K_tw_explore_kw]
-        exploration_kw_wordcount_df = exploration_kw_data_df.explode('tokenized_text')
-        exploration_kw_wordcount_df = exploration_kw_wordcount['tokenized_text'].value_counts().rename_axis('word').reset_index(name='count_top_tweets')
-        full_random_wordcount_df = pd.read_parquet('/scratch/mt4493/twitter_labor/twitter-labor-data/data/wordcount_random/wordcount_random.parquet')
-        exploration_kw_wordcount_df = exploration_kw_wordcount_df.join(full_random_wordcount_df, on=['word'])
-        exploration_kw_wordcount_df['lift'] = (exploration_kw_wordcount_df['count_top_tweets'] / exploration_kw_wordcount_df['count']) * N_random/label2rank[column]
-        exploration_kw_wordcount_df = exploration_kw_wordcount_df.sort_values(by=["lift"], ascending=False).reset_index()
-        selected_keywords_list = exploration_kw_wordcount_df['word'][:args.K_kw_explore].tolist()
+        # explore (w/ keyword lift; final data in explore_kw_data_df)
+        nb_tweets_per_keyword = int(args.N_explore_kw / args.K_kw_explore)
+        explore_kw_data_df = all_data_df[:args.K_tw_explore_kw]
+        explore_kw_wordcount_df = explore_kw_data_df.explode('tokenized_text')
+        explore_kw_wordcount_df = explore_kw_wordcount['tokenized_text'].value_counts().rename_axis(
+            'word').reset_index(name='count_top_tweets')
+        full_random_wordcount_df = pd.read_parquet(
+            '/scratch/mt4493/twitter_labor/twitter-labor-data/data/wordcount_random/wordcount_random.parquet')
+        explore_kw_wordcount_df = explore_kw_wordcount_df.join(full_random_wordcount_df, on=['word'])
+        explore_kw_wordcount_df['lift'] = (explore_kw_wordcount_df['count_top_tweets'] /
+                                               explore_kw_wordcount_df['count']) * N_random / label2rank[column]
+        explore_kw_wordcount_df = explore_kw_wordcount_df.sort_values(by=["lift"],
+                                                                              ascending=False).reset_index()
+        selected_keywords_list = explore_kw_wordcount_df['word'][:args.K_kw_explore].tolist()
+        for selected_keyword in selected_keywords_list:
+            tweets_containing_keyword_df = all_data_df[all_data_df['text'].str.contains(keyword)]
+            if tweets_containing_keyword_df.shape[0] < nb_tweets_per_keyword:
+                print("Only {} tweets containing keyword {} (< {}). Sending all of them to labelling.".format(
+                    str(tweets_containing_keyword_df.shape[0]), keyword, str(nb_tweets_per_keyword)))
+                explore_kw_data_df = tweets_containing_keyword_df
+            else:
+                explore_kw_data_df = tweets_containing_keyword_df.sample(n=nb_tweets_per_keyword)
 
-
-
-        # exploration (attention version)
-        #exploration_kw_data_df = all_data_df[:args.K_tw_explore_kw]
-        #nb_tweets_per_keyword = args.N_explore_kw // (args.K_tw_explore_kw * args.K_kw_explore)
-        #for tweet_rank in range(exploration_kw_data_df.shape[0]):
-        #    tweet_str = exploration_kw_data_df['text'][tweet_rank]
+        # explore (attention version)
+        # explore_kw_data_df = all_data_df[:args.K_tw_explore_kw]
+        # nb_tweets_per_keyword = args.N_explore_kw // (args.K_tw_explore_kw * args.K_kw_explore)
+        # for tweet_rank in range(explore_kw_data_df.shape[0]):
+        #    tweet_str = explore_kw_data_df['text'][tweet_rank]
         #    tokenized_tweet = tokenizer.tokenize(tweet)
         #    # Determine the token with the highest average attention and replace it with a [MASK] token
         #    attention_token_index = \
