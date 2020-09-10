5ec12fbf23fd5b2a97e2271a8c6db3ac6300ddbe
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-09-10 14:40:36 +0200

    Save separately top tweets

--- code/2-twitter_labor/5-active_learning/preliminary/process_inference_output.py
+++ code/2-twitter_labor/5-active_learning/preliminary/process_inference_output.py
@@ -21,7 +21,19 @@ def get_args_from_command_line():
     args = parser.parse_args()
     return args
 
+
 if __name__ == "__main__":
+    # calculate base rates
+    labels = ['is_hired_1mo', 'is_unemployed', 'job_offer', 'job_search', 'lost_job_1mo']
+    base_rates = [
+        1.7342911457049017e-05,
+        0.0003534645020523677,
+        0.005604641971672389,
+        0.00015839552996469054,
+        1.455338466552472e-05]
+    N_random = 92114009
+    base_ranks = [int(x * N_random) for x in base_rates]
+    label2rank = dict(zip(labels, base_ranks))
     # Define args from command line
     args = get_args_from_command_line()
     # Import inference data
@@ -29,13 +41,18 @@ if __name__ == "__main__":
     full_random_df = pd.concat(pd.read_parquet(parquet_file, columns=['tweet_id', 'text']) for parquet_file in random_data_dir.glob('*.parquet'))
     full_random_df = full_random_df.set_index(['tweet_id'])
     print("Loaded all random data")
-    for column in ['is_hired_1mo', 'is_unemployed', 'job_offer', 'job_search', 'lost_job_1mo']:
+    for column in labels:
         print(column)
         inference_data_dir = Path(os.path.join(args.inference_output_folder, column))
         full_inference_df = pd.concat(pd.read_parquet(parquet_file) for parquet_file in inference_data_dir.glob('*.parquet'))
         full_inference_with_text_df = full_inference_df.join(full_random_df)
         output_parquet_path = os.path.join(args.inference_output_folder, column, "{}_all.parquet".format(column))
         full_inference_with_text_df.to_parquet(output_parquet_path)
+        top_output_parquet_path = os.path.join(args.inference_output_folder, column, "{}_top.parquet".format(column))
+        full_inference_with_text_df = full_inference_with_text_df[:label2rank[column]]
+        full_inference_with_text_df.to_parquet(top_output_parquet_path)
         print("All data with text and scores for label {} saved at {}".format(column, output_parquet_path))
+        print("Top tweets with text and scores for label {} saved at {}".format(column, top_output_parquet_path))
+
 
 
