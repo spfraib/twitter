e2f98d01ec90c64386da57186673ac544cd16ac3
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-09-28 12:17:41 +0200

    Update spark script and bash

--- code/2-twitter_labor/5-active_learning/preliminary/process_inference_output_spark.py
+++ code/2-twitter_labor/5-active_learning/preliminary/process_inference_output_spark.py
@@ -3,7 +3,7 @@ import pyspark.sql.functions as F
 from pyspark.ml.feature import Bucketizer, QuantileDiscretizer
 from pyspark.sql import Window
 from pyspark.sql.types import *
-
+import subprocess
 
 def get_args_from_command_line():
     """Parse the command line arguments."""
@@ -16,6 +16,17 @@ def get_args_from_command_line():
                         default="/scratch/mt4493/twitter_labor/twitter-labor-data/data/random_chunks_with_operations")
     args = parser.parse_args()
     return args
+
+def run_cmd(args_list):
+    """
+    run linux commands
+    """
+    # import subprocess
+    print('Running system command: {0}'.format(' '.join(args_list)))
+    proc = subprocess.Popen(args_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    s_output, s_err = proc.communicate()
+    s_return = proc.returncode
+    return s_return, s_output, s_err
 
 # Define base rates
 labels = ['is_hired_1mo', 'is_unemployed', 'job_offer', 'job_search', 'lost_job_1mo']
@@ -35,14 +46,12 @@ if __name__ == "__main__":
     random_tweets_df = spark.read.parquet(args.random_chunks_with_operations_folder)
     for column in labels:
         # read inference data, perform join and isolate top tweets
-        inference_df = spark.read.parquet(args.inference_output_folder)
+        inference_df = spark.read.parquet(os.path.join(args.inference_output_folder, column))
         inference_with_text_df = inference_df.join(random_tweets_df, on='tweet_id')
         top_tweets_df = inference_with_text_df.sort(F.col("score").desc()).limit(label2rank[column])
         # prepare paths and save
         output_folder_path = os.path.join(args.inference_output_folder, 'joined', column)
         top_tweets_output_folder_path = os.path.join(output_folder_path, f"top_tweets_{column}")
-        if not os.path.exists(top_tweets_output_folder_path):
-            print('creating save folder as it does not exist', top_tweets_output_folder_path)
-            os.makedirs(top_tweets_output_folder_path)
+        run_cmd(['hdfs','dfs','-mkdir','-p',top_tweets_output_folder_path])
         top_tweets_df.coalesce(1).write.mode("overwrite").parquet(top_tweets_output_folder_path)
         inference_with_text_df.write.mode("overwrite").parquet(output_folder_path)
--- code/2-twitter_labor/5-active_learning/preliminary/process_inference_output_spark_bash.sh
+++ code/2-twitter_labor/5-active_learning/preliminary/process_inference_output_spark_bash.sh
@@ -9,17 +9,18 @@ export PYSPARK_DRIVER_PYTHON=/share/apps/python/3.6.5/bin/python
 export PYTHONIOENCODING=utf8
 
 hdfs dfs -put /scratch/mt4493/twitter_labor/twitter-labor-data/data/inference/${INFERENCE_FOLDER}/output /user/mt4493/twitter/inference/${INFERENCE_FOLDER}
-
-cd /scratch/mt4493/twitter_labor/code/twitter/code/2-twitter_labor/5-active_learning/preliminary
+echo "Loaded inference data on Hadoop. Launching the PySpark script"
+CODE_FOLDER=/scratch/mt4493/twitter_labor/code/twitter/code/2-twitter_labor/5-active_learning/preliminary
 
 spark-submit --master yarn --deploy-mode cluster --conf spark.yarn.submit.waitAppCompletion=false \
 --conf spark.serializer=org.apache.spark.serializer.KryoSerializer --conf spark.speculation=false \
 --conf spark.yarn.appMasterEnv.LANG=en_US.UTF-8 --conf spark.executorEnv.LANG=en_US.UTF-8 --driver-cores 10 \
 --driver-memory 10G --conf spark.dynamicAllocation.enabled=true --conf spark.dynamicAllocation.maxExecutors=50 \
 --executor-cores 10 --executor-memory 10G \
-process_inference_output_spark.py \
+${CODE_FOLDER}/process_inference_output_spark.py \
 --inference_output_folder /user/mt4493/twitter/inference/${INFERENCE_FOLDER} \
---random_chunks_with_operations_folder /scratch/mt4493/twitter_labor/twitter-labor-data/data/random_chunks_with_operations
+--random_chunks_with_operations_folder /user/mt4493/twitter/random_chunks_with_operations
 
-
+echo "PySpark script done. Copying output data to scratch."
 hdfs dfs -get /user/mt4493/twitter/inference/${INFERENCE_FOLDER}/joined /scratch/mt4493/twitter_labor/twitter-labor-data/data/inference/${INFERENCE_FOLDER}/output/joined
+echo "Copying data finished."
