35833356403f323de01b144223dff4e51916125b
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-09-29 10:20:33 +0200

    Add while loop to wait for job to finish to copy output files to scratch

--- code/2-twitter_labor/5-active_learning/preliminary/process_inference_output_spark_bash.sh
+++ code/2-twitter_labor/5-active_learning/preliminary/process_inference_output_spark_bash.sh
@@ -11,17 +11,29 @@ export PYTHONIOENCODING=utf8
 hdfs dfs -put /scratch/mt4493/twitter_labor/twitter-labor-data/data/inference/${INFERENCE_FOLDER}/output /user/mt4493/twitter/inference/${INFERENCE_FOLDER}
 echo "Loaded inference data on Hadoop. Launching the PySpark script"
 CODE_FOLDER=/scratch/mt4493/twitter_labor/code/twitter/code/2-twitter_labor/5-active_learning/preliminary
+JOB_NAME=process_inference_output
+spark-submit --master yarn --deploy-mode cluster --name ${JOB_NAME} \
+  --conf spark.yarn.submit.waitAppCompletion=false --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
+  --conf spark.speculation=false --conf spark.yarn.appMasterEnv.LANG=en_US.UTF-8 \
+  --conf spark.executorEnv.LANG=en_US.UTF-8 --driver-cores 10 \
+  --driver-memory 10G --conf spark.dynamicAllocation.enabled=true --conf spark.dynamicAllocation.maxExecutors=50 \
+  --executor-cores 10 --executor-memory 10G \
+  ${CODE_FOLDER}/process_inference_output_spark.py \
+  --inference_output_folder /user/mt4493/twitter/inference/${INFERENCE_FOLDER} \
+  --random_chunks_with_operations_folder /user/mt4493/twitter/random_chunks_with_operations
 
-spark-submit --master yarn --deploy-mode cluster --conf spark.yarn.submit.waitAppCompletion=false \
---conf spark.serializer=org.apache.spark.serializer.KryoSerializer --conf spark.speculation=false \
---conf spark.yarn.appMasterEnv.LANG=en_US.UTF-8 --conf spark.executorEnv.LANG=en_US.UTF-8 --driver-cores 10 \
---driver-memory 10G --conf spark.dynamicAllocation.enabled=true --conf spark.dynamicAllocation.maxExecutors=50 \
---executor-cores 10 --executor-memory 10G \
-${CODE_FOLDER}/process_inference_output_spark.py \
---inference_output_folder /user/mt4493/twitter/inference/${INFERENCE_FOLDER} \
---random_chunks_with_operations_folder /user/mt4493/twitter/random_chunks_with_operations
+echo "Submitted Spark job"
 
-echo "PySpark script done. Copying output data to scratch."
+applicationId=$(yarn application -list -appStates RUNNING | awk -v tmpJob=$JOB_NAME '{ if( $2 == tmpJob) print $1 }')
+MINUTE_COUNT=0
+while [ ! -z $applicationId ]
+do
+  echo "Waiting for job $JOB_NAME (application $applicationId) to be done to transfer output files to scratch. "
+  echo "Already waited $MINUTE_COUNT minutes. "
+  sleep 60
+  MINUTE_COUNT=$((MINUTE_COUNT+1))
+  applicationId=$(yarn application -list -appStates RUNNING | awk -v tmpJob=$JOB_NAME '{ if( $2 == tmpJob) print $1 }')
+done
+echo "Job is done. Copying data."
 hdfs dfs -get /user/mt4493/twitter/inference/${INFERENCE_FOLDER}/joined /scratch/mt4493/twitter_labor/twitter-labor-data/data/inference/${INFERENCE_FOLDER}/output/joined
 echo "Copying data finished."
-
