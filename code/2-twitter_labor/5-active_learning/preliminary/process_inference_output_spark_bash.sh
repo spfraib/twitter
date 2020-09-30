51359c86e803a4637bdb035bb5ddd2170c6c384f
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-09-30 12:02:10 +0200

    Correct path

--- code/2-twitter_labor/5-active_learning/preliminary/process_inference_output_spark_bash.sh
+++ code/2-twitter_labor/5-active_learning/preliminary/process_inference_output_spark_bash.sh
@@ -8,7 +8,7 @@ export PYSPARK_PYTHON=/share/apps/python/3.6.5/bin/python
 export PYSPARK_DRIVER_PYTHON=/share/apps/python/3.6.5/bin/python
 export PYTHONIOENCODING=utf8
 
-hdfs dfs -mkdir -p /user/mt4493/twitter/inference/${INFERENCE_FOLDER}/output
+hdfs dfs -mkdir -p /user/mt4493/twitter/inference/${INFERENCE_FOLDER}
 hdfs dfs -put /scratch/mt4493/twitter_labor/twitter-labor-data/data/inference/${INFERENCE_FOLDER}/output /user/mt4493/twitter/inference/${INFERENCE_FOLDER}
 echo "Loaded inference data on Hadoop. Launching the PySpark script"
 CODE_FOLDER=/scratch/mt4493/twitter_labor/code/twitter/code/2-twitter_labor/5-active_learning/preliminary
