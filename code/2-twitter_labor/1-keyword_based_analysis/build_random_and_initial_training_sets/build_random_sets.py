c6f6943119306e021a396cdcf286260fc320bbde
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-10-07 16:47:37 +0200

    add prints

--- code/2-twitter_labor/1-keyword_based_analysis/build_random_and_initial_training_sets/build_random_sets.py
+++ code/2-twitter_labor/1-keyword_based_analysis/build_random_and_initial_training_sets/build_random_sets.py
@@ -26,20 +26,30 @@ def get_args_from_command_line():
 if __name__ == "__main__":
     # Define args from command line
     args = get_args_from_command_line()
+    print('Start running code')
     path_to_tweets = f'/user/spf248/twitter/data/timelines/historical/extract/{args.country_code}/'
     df = spark.read.orc(path_to_tweets)
+    print('Data was loaded')
     df = df.select('tweet_id', 'text', 'tweet_lang')
     country_language_dict = {'US': 'en', 'MX': 'es', 'BR': 'pt'}
     df = df.where(df.tweet_lang == country_language_dict[args.country_code]).drop('tweet_lang')
+    print(f'Selected tweets in {country_language_dict[args.country_code]}')
     # drop duplicates and RT
     df = df.drop_duplicates(subset=['text'])
+    print('Dropped duplicates')
     df = df.filter(~df.text.contains('RT '))
+    print('Dropped RTs')
     N_all = df.count()
+    print('Total number of tweets:', N_all)
     if N_all > 200000000:
         df_random_1, df_random_2 = df.randomSplit(weights=[0.5, 0.5])
+        print('Performed random split')
         df_random_1 = df_random_1.limit(100000000)
+        print('Limited first random set to 100M tweets')
         df_random_2 = df_random_2.limit(100000000)
+        print('Limited second random set to 100M tweets')
         df_random_1.write.mode("overwrite").parquet(f'/user/spf248/twitter/data/random_samples/{args.country_code}/random_1')
+        print('Outputted first random set')
         df_random_2.write.mode("overwrite").parquet(f'/user/spf248/twitter/data/random_samples/{args.country_code}/random_2')
-
+        print('Outputted second random set')
 
