e269aecc19c86ab74d08848d9982a75b0ec6460c
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-10-09 17:16:18 +0200

    Drop duplicates when only link differ

--- code/2-twitter_labor/1-keyword_based_analysis/build_random_and_initial_training_sets/build_random_sets.py
+++ code/2-twitter_labor/1-keyword_based_analysis/build_random_and_initial_training_sets/build_random_sets.py
@@ -35,21 +35,31 @@ if __name__ == "__main__":
     df = df.where(df.tweet_lang == country_language_dict[args.country_code]).drop('tweet_lang')
     print(f'Selected tweets in {country_language_dict[args.country_code]}')
     # drop duplicates and RT
-    df = df.drop_duplicates(subset=['text'])
+    df = df.withColumn('text_no_links', F.regexp_replace('text', 'http\S+', ''))
+    df = df.drop_duplicates(subset=['text_no_links']).drop('text_no_links')
     print('Dropped duplicates')
     df = df.filter(~df.text.contains('RT '))
     print('Dropped RTs')
     N_all = df.count()
     print('Total number of tweets:', N_all)
-    if N_all > 200000000:
-        df_random_1, df_random_2 = df.randomSplit(weights=[0.5, 0.5])
-        print('Performed random split')
-        df_random_1 = df_random_1.sample(False, 100000000/df_random_1.count(), seed=0)
-        print('Limited first random set to 100M tweets')
-        df_random_2 = df_random_2.sample(False, 100000000/df_random_2.count(), seed=0)
-        print('Limited second random set to 100M tweets')
-        df_random_1.coalesce(1000).write.mode("overwrite").parquet(f'/user/spf248/twitter/data/random_samples/{args.country_code}/random_1')
-        print('Outputted first random set')
-        df_random_2.coalesce(1000).write.mode("overwrite").parquet(f'/user/spf248/twitter/data/random_samples/{args.country_code}/random_2')
-        print('Outputted second random set')
+    N_sample = 200000000
+    pct_sample = min(N_sample / N_all, 1)
+    df_random = df.sample(False, pct_sample, seed=0)
+    print('Random set to 200M tweets')
+    # Add lowercased column
+    df_random = df_random.withColumn('text_lowercase', lower(col('text')))
+    # Accent replacement for spanish and portuguese
+    accent_replacements = [
+        ('á|à|ã', 'a'),
+        ('é|ê|è', 'e'),
+        ('í', 'i'),
+        ('ò|ó|õ', 'o'),
+        ('ú|ü', 'u'),
+        ('ñ', 'n'),
+        ('ç', 'c')]
+    if args.country_code in ['MX', 'BR']:
+        for a, b in accent_replacements:
+            df_random = df_random.withColumn('text_lowercase', regexp_replace(col('text_lowercase'), a, b))
+    df_random.coalesce(2000).write.mode("overwrite").parquet(
+        f'/user/spf248/twitter/data/random_samples/{args.country_code}')
 
