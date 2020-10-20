9f3680a42ca05ba814726ec825be9d1bf9c93f2b
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-10-20 17:28:15 +0200

    Extract keys from scratch

--- code/2-twitter_labor/1-training_data_preparation/mturk/create_tasks.py
+++ code/2-twitter_labor/1-training_data_preparation/mturk/create_tasks.py
@@ -2,9 +2,15 @@ import boto3
 
 MTURK_SANDBOX = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
 
+with open('/scratch/mt4493/twitter_labor/twitter-labor-data/data/mturk/keys/access_key_id.txt', 'r') as f:
+    access_key_id = f.readline()
+
+with open('/scratch/mt4493/twitter_labor/twitter-labor-data/data/mturk/keys/secret_access_key.txt', 'r') as f:
+    secret_access_key = f.readline()
+
 mturk = boto3.client('mturk',
-                     aws_access_key_id=,
-                     aws_secret_access_key=,
+                     aws_access_key_id=access_key_id,
+                     aws_secret_access_key=secret_access_key,
                      region_name='us-east-1',
                      endpoint_url=MTURK_SANDBOX
                      )
