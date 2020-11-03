e6a5abf85784b6c981d789adcfc6635e6171393d
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-11-03 14:27:43 +0100

    Make it compatible with non-US countries

--- code/2-twitter_labor/1-training_data_preparation/mturk/assign_qualification_to_past_respondents_bots.py
+++ code/2-twitter_labor/1-training_data_preparation/mturk/assign_qualification_to_past_respondents_bots.py
@@ -14,6 +14,16 @@ import reverse_geocoder as rg
 import boto3
 
 
+def get_args_from_command_line():
+    """Parse the command line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--country_code", type=str,
+                        help="Country code",
+                        default="US")
+    args = parser.parse_args()
+    return args
+
+
 def exportSurvey(apiToken, surveyId, dataCenter, fileFormat):
     surveyId = surveyId
     fileFormat = fileFormat
@@ -57,102 +67,108 @@ def exportSurvey(apiToken, surveyId, dataCenter, fileFormat):
         os.path.join(path_to_data, country_code, "labeling", surveyId))
     print('Complete')
 
+
 def select_paths(x):
     if x == 'labor-market-tweets.csv':
         return x
-    elif 'US' in x:
+    elif any(xs in x for xs in ['US', 'BR', 'MX']):
         return x
     else:
         return None
 
-country_code = 'US'
-path_to_data = '/scratch/mt4493/twitter_labor/twitter-labor-data/data/qualtrics'
 
-with open(os.path.join(path_to_data,'keys/apiToken.txt'),'r') as f:
-    apiToken = f.readline()
+if __name__ == '__main__':
+    args = get_args_from_command_line()
+    country_code = args.country_code
+    path_to_data = '/scratch/mt4493/twitter_labor/twitter-labor-data/data/qualtrics'
 
-dataCenter = "nyu.ca1"
-fileFormat = "csv"
+    with open(os.path.join(path_to_data, 'keys/apiToken.txt'), 'r') as f:
+        apiToken = f.readline()
 
-survey_ids_list = glob(os.path.join(path_to_data, country_code,'labeling','*'))
+    dataCenter = "nyu.ca1"
+    fileFormat = "csv"
 
-print(survey_ids_list)
+    survey_ids_list = glob(os.path.join(path_to_data, country_code, 'labeling', '*'))
 
-worker_id_list = list()
-for surveyId in survey_ids_list:
-    if not os.path.exists(os.path.join(path_to_data, country_code,'labeling',surveyId)):
-        if not re.compile('^SV_.*').match(surveyId):
-            print("survey Id must match ^SV_.*")
-        else:
-            exportSurvey(apiToken, surveyId, dataCenter, fileFormat)
-    folder_path = os.path.join(path_to_data, country_code,'labeling',surveyId)
-    file_path_list = os.listdir(folder_path)
-    file_path_list = [select_paths(path) for path in file_path_list if select_paths(path) is not None]
-    if len(file_path_list) > 0:
-        df=pd.read_csv(os.path.join(folder_path, file_path_list[0]),low_memory=False)
+    print(survey_ids_list)
 
-        # First two rows contain metadata
-        df.drop([0,1],inplace=True)
+    worker_id_list = list()
+    for surveyId in survey_ids_list:
+        if not os.path.exists(os.path.join(path_to_data, country_code, 'labeling', surveyId)):
+            if not re.compile('^SV_.*').match(surveyId):
+                print("survey Id must match ^SV_.*")
+            else:
+                exportSurvey(apiToken, surveyId, dataCenter, fileFormat)
+        folder_path = os.path.join(path_to_data, country_code, 'labeling', surveyId)
+        file_path_list = os.listdir(folder_path)
+        file_path_list = [select_paths(path) for path in file_path_list if select_paths(path) is not None]
+        if len(file_path_list) > 0:
+            df = pd.read_csv(os.path.join(folder_path, file_path_list[0]), low_memory=False)
 
-        df=df.loc[(df['QIDWorker'].dropna().drop_duplicates().index)].set_index('QIDWorker').copy()
+            # First two rows contain metadata
+            df.drop([0, 1], inplace=True)
 
-        #places=rg.search([tuple(x) for x in df[['LocationLatitude','LocationLongitude']].astype(float).dropna().values.tolist()])
+            df = df.loc[(df['QIDWorker'].dropna().drop_duplicates().index)].set_index('QIDWorker').copy()
 
-        print('# of workers who refused the consent form:', (df.QIDConsent.astype(int)==0).sum())
-        print('# of workers who did not complete the survey:', (df.Finished.astype(int)==0).sum())
+            # places=rg.search([tuple(x) for x in df[['LocationLatitude','LocationLongitude']].astype(float).dropna().values.tolist()])
 
-        to_drop=[
-        'ResponseID',
-        'ResponseSet',
-        'IPAddress',
-        'StartDate',
-        'EndDate',
-        'RecipientLastName',
-        'RecipientFirstName',
-        'RecipientEmail',
-        'ExternalDataReference',
-        'Finished',
-        'Status',
-        'Random ID',
-        'QIDConsent',
-        'QIDDescription',
-        'QIDCompletion',
-        'LocationLatitude',
-        'LocationLongitude',
-        'LocationAccuracy']
+            print('# of workers who refused the consent form:', (df.QIDConsent.astype(int) == 0).sum())
+            print('# of workers who did not complete the survey:', (df.Finished.astype(int) == 0).sum())
 
-        df.drop(to_drop,1,inplace=True,errors='ignore')
-        df.drop([x for x in df.columns if 'BR-FL_' in x],1,inplace=True,errors='ignore')
-        df = df.reset_index()
-        df = df[df['QIDWorker'].notna()].reset_index(drop=True)
-        worker_id_list = worker_id_list +  df['QIDWorker'].tolist()
+            to_drop = [
+                'ResponseID',
+                'ResponseSet',
+                'IPAddress',
+                'StartDate',
+                'EndDate',
+                'RecipientLastName',
+                'RecipientFirstName',
+                'RecipientEmail',
+                'ExternalDataReference',
+                'Finished',
+                'Status',
+                'Random ID',
+                'QIDConsent',
+                'QIDDescription',
+                'QIDCompletion',
+                'LocationLatitude',
+                'LocationLongitude',
+                'LocationAccuracy']
 
-print(len(worker_id_list))
+            df.drop(to_drop, 1, inplace=True, errors='ignore')
+            df.drop([x for x in df.columns if 'BR-FL_' in x], 1, inplace=True, errors='ignore')
+            df = df.reset_index()
+            df = df[df['QIDWorker'].notna()].reset_index(drop=True)
+            worker_id_list = worker_id_list + df['QIDWorker'].tolist()
 
-# Assign qualification
+    print(len(worker_id_list))
 
-keys_path = '/scratch/mt4493/twitter_labor/twitter-labor-data/data/mturk/keys'
-with open(os.path.join(keys_path, 'access_key_id.txt'), 'r') as f:
-    access_key_id = f.readline().strip()
+    # Remove duplicates
+    worker_id_list = list(dict.fromkeys(worker_id_list))
+    # Assign qualification
 
-with open(os.path.join(keys_path, 'secret_access_key.txt'), 'r') as f:
-    secret_access_key = f.readline().strip()
+    keys_path = '/scratch/mt4493/twitter_labor/twitter-labor-data/data/mturk/keys'
+    with open(os.path.join(keys_path, 'access_key_id.txt'), 'r') as f:
+        access_key_id = f.readline().strip()
 
-mturk = boto3.client('mturk',
-                     aws_access_key_id=access_key_id,
-                     aws_secret_access_key=secret_access_key,
-                     region_name='us-east-1',
-                     endpoint_url='https://mturk-requester.us-east-1.amazonaws.com'
-                     )
+    with open(os.path.join(keys_path, 'secret_access_key.txt'), 'r') as f:
+        secret_access_key = f.readline().strip()
 
-for worker_id in worker_id_list:
-    try:
-        mturk.associate_qualification_with_worker(
-        QualificationTypeId='3YLTB9JB8TED72KIAHT6K4NASKY63F',
-        WorkerId=worker_id,
-        IntegerValue=1,
-        SendNotification=False)
-    except:
-        print(worker_id)
+    mturk = boto3.client('mturk',
+                         aws_access_key_id=access_key_id,
+                         aws_secret_access_key=secret_access_key,
+                         region_name='us-east-1',
+                         endpoint_url='https://mturk-requester.us-east-1.amazonaws.com'
+                         )
 
-print("The Qualification was assigned to all workers who already completed the US survey (including bots). ")
+    for worker_id in worker_id_list:
+        try:
+            mturk.associate_qualification_with_worker(
+                QualificationTypeId='3YLTB9JB8TED72KIAHT6K4NASKY63F',
+                WorkerId=worker_id,
+                IntegerValue=1,
+                SendNotification=False)
+        except:
+            print(worker_id)
+
+    print("The Qualification was assigned to all workers who already completed the US survey (including bots). ")
