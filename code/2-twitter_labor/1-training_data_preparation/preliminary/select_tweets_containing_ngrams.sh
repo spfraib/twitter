3811cb68b0b4ca664570e2ae4a47bdc752875f20
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-11-03 15:05:41 +0100

    Modify paths after reorganization

--- code/2-twitter_labor/1-training_data_preparation/preliminary/select_tweets_containing_ngrams.sh
+++ code/2-twitter_labor/1-training_data_preparation/preliminary/select_tweets_containing_ngrams.sh
@@ -9,7 +9,7 @@ export PYSPARK_PYTHON=/share/apps/python/3.6.5/bin/python
 export PYSPARK_DRIVER_PYTHON=/share/apps/python/3.6.5/bin/python
 export PYTHONIOENCODING=utf8
 
-CODE_FOLDER=/scratch/mt4493/twitter_labor/code/twitter/code/2-twitter_labor/1-training_data_preparation
+CODE_FOLDER=/scratch/mt4493/twitter_labor/code/twitter/code/2-twitter_labor/1-training_data_preparation/preliminary
 TIMESTAMP=$(date +%s)
 JOB_NAME=select_tweets_containing_ngrams_${TIMESTAMP}
 spark-submit --master yarn --deploy-mode cluster --name ${JOB_NAME} \
