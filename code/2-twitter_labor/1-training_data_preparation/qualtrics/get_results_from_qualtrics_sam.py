3e22d3c039587303d52cf7b815a57c5d6153170f
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-11-04 10:24:50 +0100

    Delete useless function

--- code/2-twitter_labor/1-training_data_preparation/qualtrics/get_results_from_qualtrics_sam.py
+++ code/2-twitter_labor/1-training_data_preparation/qualtrics/get_results_from_qualtrics_sam.py
@@ -24,50 +24,6 @@ def get_args_from_command_line():
     return args
 
 
-def exportSurvey(apiToken, surveyId, dataCenter, fileFormat, path_to_data):
-    surveyId = surveyId
-    fileFormat = fileFormat
-    dataCenter = dataCenter
-
-    # Setting static parameters
-    requestCheckProgress = 0.0
-    progressStatus = "inProgress"
-    baseUrl = "https://{0}.qualtrics.com/API/v3/responseexports/".format(dataCenter)
-    headers = {
-        "content-type": "application/json",
-        "x-api-token": apiToken,
-    }
-
-    # Step 1: Creating Data Export
-    downloadRequestUrl = baseUrl
-    downloadRequestPayload = '{"format":"' + fileFormat + '","surveyId":"' + surveyId + '"}'
-    downloadRequestResponse = requests.request("POST", downloadRequestUrl, data=downloadRequestPayload, headers=headers)
-    progressId = downloadRequestResponse.json()["result"]['id']
-    print(downloadRequestResponse.text)
-
-    # Step 2: Checking on Data Export Progress and waiting until export is ready
-    while progressStatus != "complete" and progressStatus != "failed":
-        print("progressStatus=", progressStatus)
-        requestCheckUrl = baseUrl + progressId
-        requestCheckResponse = requests.request("GET", requestCheckUrl, headers=headers)
-        requestCheckProgress = requestCheckResponse.json()["result"]["percentComplete"]
-        print("Download is " + str(requestCheckProgress) + " complete")
-        progressStatus = requestCheckResponse.json()["result"]["status"]
-
-    # step 2.1: Check for error
-    if progressStatus is "failed":
-        raise Exception("export failed")
-
-    # # Step 3: Downloading file
-    requestDownloadUrl = baseUrl + progressId + '/file'
-    requestDownload = requests.request("GET", requestDownloadUrl, headers=headers, stream=True)
-
-    # Step 4: Unzipping the file
-    zipfile.ZipFile(io.BytesIO(requestDownload.content)).extractall(
-        os.path.join(path_to_data, surveyId))
-    print('Complete')
-
-
 def fill_assignment_worker_ids_dict(assignments_dict, assignment_worker_ids_dict):
     for assignment_nb in range(len(assignments_dict['Assignments'])):
         assignment_info_dict = assignments_dict['Assignments'][assignment_nb]
