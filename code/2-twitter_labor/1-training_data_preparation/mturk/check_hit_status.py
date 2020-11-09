e84f54db59347f1685652840e81f189402f35659
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-11-09 15:54:26 +0100

    Update to latest version

---
+++ code/2-twitter_labor/1-training_data_preparation/mturk/check_hit_status.py
@@ -0,0 +1,38 @@
+import boto3
+import os
+import argparse
+
+def get_args_from_command_line():
+    """Parse the command line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--HITId", type=str)
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    # Get args
+    args = get_args_from_command_line()
+    # Load mturk client
+    keys_path = '/scratch/mt4493/twitter_labor/twitter-labor-data/data/mturk/keys'
+    with open(os.path.join(keys_path, 'access_key_id.txt'), 'r') as f:
+        access_key_id = f.readline().strip()
+
+    with open(os.path.join(keys_path, 'secret_access_key.txt'), 'r') as f:
+        secret_access_key = f.readline().strip()
+
+    mturk = boto3.client('mturk',
+                         aws_access_key_id=access_key_id,
+                         aws_secret_access_key=secret_access_key,
+                         region_name='us-east-1',
+                         endpoint_url='https://mturk-requester.us-east-1.amazonaws.com'
+                         )
+
+    # Get hit status and print
+    response = mturk.get_hit(HITId=args.HITId)
+    print(response)
+
+if __name__ == '__main__':
+    main()
+
+    
