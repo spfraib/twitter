e0865b0256627570d1399cd2813352979265b7f6
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-11-10 12:25:16 +0100

    Update script and bash file

---
+++ code/1-data_preparation/3-users/get-users-profiles-pyspark.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+module purge
+module load emoji/mt4493-20201106
+module load spark/2.4.0
+export PYSPARK_PYTHON=/share/apps/python/3.6.5/bin/python
+export PYSPARK_DRIVER_PYTHON=/share/apps/python/3.6.5/bin/python
+export PYTHONIOENCODING=utf8
+
+CODE_FOLDER=/scratch/mt4493/twitter_labor/code/twitter/code/1-data_preparation/3-users
+NB_CORES=20
+TIMESTAMP=$(date +%s)
+JOB_NAME=get_users_profiles_${TIMESTAMP}
+spark-submit --master yarn --deploy-mode cluster --name ${JOB_NAME} \
+  --conf spark.yarn.submit.waitAppCompletion=false --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
+  --conf spark.speculation=false --conf spark.yarn.appMasterEnv.LANG=en_US.UTF-8 \
+  --conf spark.executorEnv.LANG=en_US.UTF-8 --driver-cores ${NB_CORES} \
+  --driver-memory ${NB_CORES}G --conf spark.dynamicAllocation.enabled=true --conf spark.dynamicAllocation.maxExecutors=50 \
+  --executor-cores ${NB_CORES} --executor-memory ${NB_CORES}G \
+  ${CODE_FOLDER}/get-users-profiles-pyspark.py \
+  --
+
+
+echo "Submitted Spark job"
