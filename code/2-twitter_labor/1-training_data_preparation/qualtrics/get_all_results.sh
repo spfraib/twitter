d05275ec7df28949a48e33dba3b1ac88dde949c4
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-11-16 17:51:31 +0100

    Add latest changes

--- code/2-twitter_labor/1-training_data_preparation/qualtrics/get_all_results.sh
+++ code/2-twitter_labor/1-training_data_preparation/qualtrics/get_all_results.sh
@@ -1,50 +1,63 @@
 #!/bin/bash
 
-declare -a StringArray=('SV_8bS2BjePnBf6XaZ'
- 'SV_3EtNuuwzOsM6yO1' 
- 'SV_6JrZIYf8DQSo8F7' 
- 'SV_eCX1MoB31N4OwO9' 
- 'SV_cx9EpYoXVN3p1sx' 
- 'SV_eWkEUtG1ktVE1jD' 
- 'SV_2tKBgJaqDKz4VkF' 
- 'SV_5nIw8CwF1v0128B' 
- 'SV_9TabupxnmxtlyL3' 
- 'SV_b78uXE4TG3vGwC1' 
- 'SV_7VxaoBOsRCaZo9L' 
- 'SV_cTG8uiAFsVPTOq9' 
- 'SV_38GbM3bSN2lsq8J' 
- 'SV_7a0R6QnfIYBZO7j' 
- 'SV_b3NNaILgrFStdzL' 
- 'SV_2lb5YiRSq9f59jf' 
- 'SV_eEFLCnc2FT50QD3' 
- 'SV_00wHtonmOcGzrx3' 
- 'SV_8czHeb2J0p55UiN' 
- 'SV_eD2m4MWNYdIwH2J')
+COUNTRY_CODE=$1
+DISCARD_X=$2
 
-DATA_PATH=/scratch/mt4493/twitter_labor/twitter-labor-data/data/qualtrics/US/labeling
-
-cd ${DATA_PATH}
-
-if [ -f new_labels.pkl ]; then
-  rm new_labels.pkl
-  echo 'Removed new_labels.pkl'
-fi
-
-
-
-for val in ${StringArray[@]}; do
-  if [ -d ${val} ]; then
-    rm -rf ${val}
-    echo "Removed ${val}"
-  fi
-done
-
+DATA_PATH=/scratch/mt4493/twitter_labor/twitter-labor-data/data/qualtrics/${COUNTRY_CODE}/labeling
 CODE_PATH=/scratch/mt4493/twitter_labor/code/twitter/code/2-twitter_labor/1-training_data_preparation/qualtrics
 
-cd ${CODE_PATH}
-for val in ${StringArray[@]}; do
-    sbatch get_results_from_qualtrics.sbatch US ${val} 0 0
-    echo "Launched sbatch for ${val}"
-    sleep 60
-done
+if [ $COUNTRY_CODE == "US" ]; then
+  declare -a StringArray=('SV_8bS2BjePnBf6XaZ'
+   'SV_3EtNuuwzOsM6yO1'
+   'SV_6JrZIYf8DQSo8F7'
+   'SV_eCX1MoB31N4OwO9'
+   'SV_cx9EpYoXVN3p1sx'
+   'SV_eWkEUtG1ktVE1jD'
+   'SV_2tKBgJaqDKz4VkF'
+   'SV_5nIw8CwF1v0128B'
+   'SV_9TabupxnmxtlyL3'
+   'SV_b78uXE4TG3vGwC1'
+   'SV_7VxaoBOsRCaZo9L'
+   'SV_cTG8uiAFsVPTOq9'
+   'SV_38GbM3bSN2lsq8J'
+   'SV_7a0R6QnfIYBZO7j'
+   'SV_b3NNaILgrFStdzL'
+   'SV_2lb5YiRSq9f59jf'
+   'SV_eEFLCnc2FT50QD3'
+   'SV_00wHtonmOcGzrx3'
+   'SV_8czHeb2J0p55UiN'
+   'SV_eD2m4MWNYdIwH2J')
 
+  cd ${DATA_PATH}
+
+  if [ -f new_labels.pkl ]; then
+    rm new_labels.pkl
+    echo 'Removed new_labels.pkl'
+  fi
+
+
+
+  for val in ${StringArray[@]}; do
+    if [ -d ${val} ]; then
+      rm -rf ${val}
+      echo "Removed ${val}"
+    fi
+  done
+
+  cd ${CODE_PATH}
+  for val in ${StringArray[@]}; do
+      sbatch get_results_from_qualtrics.sbatch US ${val} 0 0 ${DISCARD_X}
+      echo "Launched sbatch for ${val}"
+      sleep 60
+  done
+
+else
+  rm -rf ${DATA_PATH}
+  mkdir ${DATA_PATH}
+
+  cd ${CODE_PATH}
+  for val in ${StringArray[@]}; do
+      sbatch get_results_from_qualtrics.sbatch ${COUNTRY_CODE} ${val} 0 0 ${DISCARD_X}
+      echo "Launched sbatch for ${val}"
+      sleep 60
+  done
