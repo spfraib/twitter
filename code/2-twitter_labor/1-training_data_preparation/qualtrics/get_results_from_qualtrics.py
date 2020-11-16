1b6c0cfc8bcd4034d68dc3f4f3a37c59f8c16a11
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-11-16 17:11:34 +0100

    Modify label selection

--- code/2-twitter_labor/1-training_data_preparation/qualtrics/get_results_from_qualtrics.py
+++ code/2-twitter_labor/1-training_data_preparation/qualtrics/get_results_from_qualtrics.py
@@ -210,7 +210,7 @@ if __name__ == "__main__":
         level='check_id').unstack(
         level='class_id').fillna('').apply(
         lambda x: '_'.join(x), 1).apply(is_bot).where(
-        lambda x: x < 3).dropna().index
+        lambda x: x < 2).dropna().index
 
     if args.reject_bots == 1:
         keys_path = '/scratch/mt4493/twitter_labor/twitter-labor-data/data/keys/mturk'
