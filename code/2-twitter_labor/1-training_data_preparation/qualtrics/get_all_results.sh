472e888ded1517ab9fdf1b9a37a355e2b443cda4
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-11-16 17:52:15 +0100

    Add echo

--- code/2-twitter_labor/1-training_data_preparation/qualtrics/get_all_results.sh
+++ code/2-twitter_labor/1-training_data_preparation/qualtrics/get_all_results.sh
@@ -54,7 +54,8 @@ if [ $COUNTRY_CODE == "US" ]; then
 else
   rm -rf ${DATA_PATH}
   mkdir ${DATA_PATH}
-
+  echo "Removed the folder ${DATA_PATH} and recreated it"
+  
   cd ${CODE_PATH}
   for val in ${StringArray[@]}; do
       sbatch get_results_from_qualtrics.sbatch ${COUNTRY_CODE} ${val} 0 0 ${DISCARD_X}
