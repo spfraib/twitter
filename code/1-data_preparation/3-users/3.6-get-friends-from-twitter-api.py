ce48eb380e7d03f916f1512cf6999689eb7aeb8f
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-11-21 16:44:46 +0100

    Cutoff to 250

--- code/1-data_preparation/3-users/3.6-get-friends-from-twitter-api.py
+++ code/1-data_preparation/3-users/3.6-get-friends-from-twitter-api.py
@@ -32,7 +32,7 @@ def get_args_from_command_line():
     """Parse the command line arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument("--cutoff", type=int,
-                        help="The number of timelines to download before saving", default=1000)
+                        help="The number of timelines to download before saving", default=250)
     parser.add_argument("--country_code", type=str)
 
     args = parser.parse_args()
