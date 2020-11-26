0a08fce32ae0b6c5edca00a668109ce38dbcbf7d
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-11-26 18:48:16 +0100

    Minor changes

--- code/2-twitter_labor/2-model_training/bert_models/benchmark.py
+++ code/2-twitter_labor/2-model_training/bert_models/benchmark.py
@@ -21,7 +21,7 @@ def get_args_from_command_line():
 def build_average_auc_dict(results_dict: dict, model_type: str) -> dict:
     """
     Compute the average AUC value for each label and across models.
-    Source: https://stackoverflow.com/questions/36053321/python-average-all-key-values-with-same-key-to-dictionary
+    Adapted from https://stackoverflow.com/questions/36053321/python-average-all-key-values-with-same-key-to-dictionary
     """
     by_label = defaultdict(list)
     for model, mrs in results_dict[model_type].items():
@@ -34,7 +34,8 @@ def compare_model_results(results_dict_1: dict, results_dict_2: dict, model_type
     """ Compare average AUC results and print best results for each label."""
     for label in ['lost_job_1mo', 'is_hired_1mo', 'is_unemployed', 'job_offer', 'job_search']:
         label_key = f'auc_{label}'
-        diff = abs((results_dict_1[label_key] - results_dict_2[label_key]) / results_dict_1[label_key])
+        diff = abs((results_dict_1[label_key] - results_dict_2[label_key]) / results_dict_1[label_key])*100
+        logging.info('\n')
         if results_dict_1[label_key] > results_dict_2[label_key]:
             logging.info(f'Model {model_types_list[0]} better for label {label} by {diff}%')
             logging.info(f'Best AUC for label {label}: {results_dict_1[label_key]}')
@@ -46,9 +47,12 @@ def compare_model_results(results_dict_1: dict, results_dict_2: dict, model_type
 if __name__ == '__main__':
     args = get_args_from_command_line()
     results_folders_list = os.listdir(args.results_folder)
+    print(results_folders_list)
     job_ids_list = [x.split('_')[1] for x in results_folders_list]
+    print(job_ids_list)
     # keep 10 latest jobs
     indexes_to_keep_list = sorted(range(len(job_ids_list)), key=lambda x: job_ids_list[x])[-2:]
+    print(indexes_to_keep_list)
     results_dict = dict()
     model_types_list = list()
     for index in indexes_to_keep_list:
@@ -64,6 +68,8 @@ if __name__ == '__main__':
                              index_col=0)
             results_dict[model_type_str][result_folder_name_str][f'auc_{label}'] = float(df['value']['auc'])
     # get average AUC for each label and model type
+    if len(model_types_list) != 2:
+        logging.error("More than two models are being compared.")
     average_auc_model_1_dict = build_average_auc_dict(results_dict=results_dict, model_type=model_types_list[0])
     average_auc_model_2_dict = build_average_auc_dict(results_dict=results_dict, model_type=model_types_list[1])
     compare_model_results(results_dict_1=average_auc_model_1_dict, results_dict_2=average_auc_model_2_dict,
