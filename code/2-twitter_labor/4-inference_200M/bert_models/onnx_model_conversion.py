e68f4c1598a64cb5f81f2ebee354d4c1536b430e
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-12-08 09:53:52 +0100

    Modify ONNX conversion for other models

--- code/2-twitter_labor/4-inference_200M/bert_models/onnx_model_conversion.py
+++ code/2-twitter_labor/4-inference_200M/bert_models/onnx_model_conversion.py
@@ -18,13 +18,23 @@ from transformers.convert_graph_to_onnx import convert
 from transformers import BertConfig, BertTokenizer, BertTokenizerFast, BertForSequenceClassification
 from onnxruntime_tools import optimizer
 
-model_path_from_terminal = sys.argv[1] 
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--model_path", help="path to pytorch models (with onnx model in model_path/onnx/")
+parser.add_argument("--model_name", help="Name of model in HF model hub")
+parser.add_argument("--model_type", help="Model type (BERT, RoBERTa & co.)")
+
+args = parser.parse_args()
+
+model_path_from_terminal = sys.argv[1]
 # e.g. '/scratch/mt4493/2-twitter_labor/trained_models/iter0/jul23_iter0/DeepPavlov_bert-base-cased-conversational_jul23_iter0_preprocessed_11232989/{}/models/best_model'
 
 for label in ["lost_job_1mo","is_unemployed", "job_search", "is_hired_1mo", "job_offer"]:
 
     print('*****************************{}*****************************'.format(label))
-    model_path = model_path_from_terminal.format(label)
+    model_path = args.model_path.format(label)
     onnx_path = model_path+'/onnx/'.format(label)
 
     try:
@@ -36,14 +46,14 @@ for label in ["lost_job_1mo","is_unemployed", "job_search", "is_hired_1mo", "job
     print('>> converting..')
     convert(framework="pt", 
         model=model_path, 
-        tokenizer="DeepPavlov/bert-base-cased-conversational",
+        tokenizer=args.model_name,
         output=onnx_path+'converted.onnx', 
         opset=11)
 
     print('>> optimizing..')
     # ONNX optimization
     optimized_model = optimizer.optimize_model(onnx_path+'/converted.onnx',
-                                               model_type='bert', 
+                                               model_type=args.model_type,
                                                num_heads=12, 
                                                hidden_size=768)
 
@@ -54,7 +64,7 @@ for label in ["lost_job_1mo","is_unemployed", "job_search", "is_hired_1mo", "job
     print('>> quantizing..')    
     model = onnx.load(onnx_path+'/converted.onnx')
     quantized_model = quantize(model=model, quantization_mode=QuantizationMode.IntegerOps, force_fusions=True, symmetric_weight=True)
-    optimized_quantized_onnx_model_path = os.path.join(os.path.dirname(optimized_onnx_model_path), 'bert_optimized_ONNXquantized.onnx')
+    optimized_quantized_onnx_model_path = os.path.join(os.path.dirname(optimized_onnx_model_path), 'ONNX_model_optimized_quantized.onnx')
     onnx.save_model(quantized_model, optimized_quantized_onnx_model_path)
     print('Quantized&optimized model saved at :', optimized_quantized_onnx_model_path)
     
