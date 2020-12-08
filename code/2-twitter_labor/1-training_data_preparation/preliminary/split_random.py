a4f3a1fe06475a3b804d6f99569248e7f3d634f5
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-12-08 11:31:02 +0100

    Correct paths

--- code/2-twitter_labor/1-training_data_preparation/preliminary/split_random.py
+++ code/2-twitter_labor/1-training_data_preparation/preliminary/split_random.py
@@ -26,15 +26,15 @@ if __name__ == "__main__":
     # Define args from command line
     args = get_args_from_command_line()
     print('Start running code')
-    path_to_tweets = f'/user/spf248/twitter/data/timelines/random_samples/{args.country_code}/'
+    path_to_tweets = f'/user/spf248/twitter/data/random_samples/{args.country_code}/'
     df = spark.read.orc(path_to_tweets)
     print('Data was loaded')
     df = df.select('tweet_id', 'text')
     pct_sample = min(100000000 / df.count(), 1.0)
     df_new_samples = df.sample(False, pct_sample, seed=0)
     df_evaluation = df.join(df_new_samples, df.tweet_id == df_new_samples.tweet_id, "left_outer").where(df_new_samples.tweet_id.isNull()).select([col(c) for c in df.columns])
-    path_to_df_new_samples = f'/user/spf248/twitter/data/timelines/random_samples/random_samples_splitted/{args.country_code}/new_samples'
-    path_to_df_evaluation = f'/user/spf248/twitter/data/timelines/random_samples/random_samples_splitted/{args.country_code}/evaluation'
+    path_to_df_new_samples = f'/user/spf248/twitter/data/random_samples/random_samples_splitted/{args.country_code}/new_samples'
+    path_to_df_evaluation = f'/user/spf248/twitter/data/random_samples/random_samples_splitted/{args.country_code}/evaluation'
     df_new_samples.write.mode("overwrite").parquet(path_to_df_new_samples)
     df_evaluation.write.mode("overwrite").parquet(path_to_df_evaluation)
 
