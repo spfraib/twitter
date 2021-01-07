050d30480e12fbbe8ace3987fce729b4205b57b7
Manuel Tonneau <manuel.tonneau@mailbox.org>
2021-01-07 10:47:10 +0100

    Simplify ONNX conversion with more recent versions of packages

---
+++ code/2-twitter_labor/4-inference_200M/bert_models/onnx_model_conversion_2021.py
@@ -0,0 +1,49 @@
+# code to convert saved pytorch models to optimized+quantized onnx models for faster inference
+
+import os
+import shutil
+from transformers.convert_graph_to_onnx import convert, optimize, quantize, verify
+import argparse
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--model_path", help="path to pytorch models (with onnx model in model_path/onnx/")
+parser.add_argument("--model_name", help="Name of model in HF model hub")
+
+args = parser.parse_args()
+
+for label in ["lost_job_1mo", "is_unemployed", "job_search", "is_hired_1mo", "job_offer"]:
+
+    logger.info(f'*****************************{label}*****************************')
+    model_path = args.model_path.format(label)
+    onnx_path = model_path + '/onnx'.format(label)
+
+    try:
+        shutil.rmtree(onnx_path)  # deleting onxx folder and contents, if exists, conversion excepts
+    except:
+        logger.info('no existing folder, creating one')
+        os.makedirs(onnx_path)
+
+    logger.info('>> converting..')
+    convert(framework="pt",
+            model=model_path,
+            tokenizer=args.model_name,
+            output=os.path.join(onnx_path, 'converted.onnx'),
+            opset=11,
+            pipeline='sentiment-analysis')
+
+    logger.info('>> ONNX optimization')
+    optimized_output = optimize(os.path.join(onnx_path, 'converted.onnx'))
+    logger.info('>> Quantization')
+    quantized_output = quantize(optimized_output)
+
+    logger.info('Verification')
+    verify(os.path.join(onnx_path, 'converted.onnx'))
+    verify(optimized_output)
+    verify(quantized_output)
