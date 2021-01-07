ff026cd5bdc80f7e36392b51ca98acc5f42c702b
Manuel Tonneau <manuel.tonneau@mailbox.org>
2021-01-07 10:47:23 +0100

    Update evaluation with new folder structure

--- code/2-twitter_labor/3-model_evaluation/evaluation.py
+++ code/2-twitter_labor/3-model_evaluation/evaluation.py
@@ -6,6 +6,7 @@ import logging
 import statistics
 import socket
 from pathlib import Path
+import re
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
@@ -17,7 +18,9 @@ def get_args_from_command_line():
     parser = argparse.ArgumentParser()
     parser.add_argument("--country_code", type=str,
                         default="US")
-    parser.add_argument("--run_name", type=str, help="Name of the output CSV containing results", default='test')
+    parser.add_argument("--data_folder", type=str,
+                        default="jan5_iter0")
+    # parser.add_argument("--run_name", type=str, help="Name of the output CSV containing results", default='test')
 
 
     args = parser.parse_args()
@@ -39,6 +42,7 @@ def build_auc_dict(results_dict: dict, model_type: str) -> dict:
                         'min': min(auc_value_list),
                         'max': max(auc_value_list)
                         } for label_key, auc_value_list in by_label.items()}
+
 
 # def output_results(results_dict: dict):
 #     """Log AUC statistics for each label"""
@@ -68,46 +72,71 @@ def build_auc_dict(results_dict: dict, model_type: str) -> dict:
 if __name__ == '__main__':
     args = get_args_from_command_line()
     if 'manuto' in socket.gethostname().lower():
-        output_path = f'/home/manuto/Documents/world_bank/bert_twitter_labor/twitter-labor-data/data/nov13_iter0/{args.country_code}/evaluation'
-        args.results_folder = f'/home/manuto/Documents/world_bank/bert_twitter_labor/twitter-labor-data/data/nov13_iter0/{args.country_code}/train_test/results'
+        output_path = f'/home/manuto/Documents/world_bank/bert_twitter_labor/twitter-labor-data/data/{args.data_folder}/{args.country_code}/evaluation'
+        results_folder = f'/home/manuto/Documents/world_bank/bert_twitter_labor/twitter-labor-data/data/{args.data_folder}/{args.country_code}/train_test/results'
     else:
-        output_path = f'/scratch/mt4493/twitter_labor/twitter-labor-data/data/nov13_iter0/{args.country_code}/evaluation'
-        args.results_folder = f'/scratch/mt4493/twitter_labor/twitter-labor-data/data/nov13_iter0/{args.country_code}/train_test/results'
+        output_path = f'/scratch/mt4493/twitter_labor/twitter-labor-data/data/{args.data_folder}/{args.country_code}/evaluation'
+        results_folder = f'/scratch/mt4493/twitter_labor/twitter-labor-data/data/{args.data_folder}/{args.country_code}/train_test/results'
 
-    results_folders_list = os.listdir(args.results_folder)
-    logger.info(results_folders_list)
-    job_ids_list = [x.split('_')[1] for x in results_folders_list]
-    logger.info(job_ids_list)
-    # keep 10 latest jobs
-    indexes_to_keep_list = sorted(range(len(job_ids_list)), key=lambda x: job_ids_list[x])[-15:]
-    logger.info(indexes_to_keep_list)
+    results_folders_list = os.listdir(results_folder)
     results_dict = dict()
-    model_types_list = list()
-    for index in indexes_to_keep_list:
-        result_folder_name_str = results_folders_list[index]
-        print(result_folder_name_str)
-        model_type_str = f"{result_folder_name_str.split('_')[0]}_{result_folder_name_str.split('_')[2]}"
-        if model_type_str not in model_types_list:
-            model_types_list.append(model_type_str)
-        if model_type_str not in results_dict.keys():
-            results_dict[model_type_str] = dict()
-        # results_dict[model_type_str] = dict()
-        for label in ['lost_job_1mo', 'is_hired_1mo', 'is_unemployed', 'job_offer', 'job_search']:
-            path_data = os.path.join(args.results_folder, result_folder_name_str, f'val_{label}_evaluation.csv')
-            if Path(path_data).exists():
-                df = pd.read_csv(os.path.join(args.results_folder, result_folder_name_str, f'holdout_{label}_evaluation.csv'),
-                                 index_col=0)
-                results_dict[model_type_str][f'auc_{label}'] = float(df['value']['auc'])
-    # get average AUC for each label and model type
-    # if len(model_types_list) != 2:
-    #     logger.error("More than two models are being compared.")
-    logger.info(model_types_list)
-    logger.info(results_dict)
-    #average_auc_model_1_dict = build_auc_dict(results_dict=results_dict, model_type=model_types_list[0])
-    results_df = pd.DataFrame.from_dict(results_dict, orient='index')
+    for model in ['roberta-base', 'DeepPavlov-bert-base-cased-conversational', 'vinai-bertweet-base']:
+        for seed in range(1,16):
+            r = re.compile(f'{model}_[0-9]+_seed-{str(seed)}$')
+            folder_name_str = list(filter(r.match, results_folders_list))[0]
+            for label in ['lost_job_1mo', 'is_hired_1mo', 'is_unemployed', 'job_offer', 'job_search']:
+                if label not in results_dict.keys():
+                    results_dict[label] = dict()
+                path_data = os.path.join(results_folder, folder_name_str, f'val_{label}_evaluation.csv')
+                if Path(path_data).exists():
+                    df = pd.read_csv(
+                        os.path.join(results_folder, folder_name_str, f'val_{label}_evaluation.csv'),
+                        index_col=0)
+                    results_dict[label][f'auc_{model}_{str(seed)}'] = float(df['value']['auc'])
+    results_df = pd.DataFrame.from_dict(results_dict)
     results_df = results_df.round(3)
-    print(results_df.head(n=15))
-    # if not os.path.exists(output_path):
-    #     os.makedirs(output_path)
-    # results_df.to_csv(os.path.join(output_path, f'{args.run_name}.csv'))
+    results_df = results_df.reset_index()
+    results_df['model'] = results_df['index'].apply(lambda x: x.split('_')[1])
+    results_df['seed'] = results_df['index'].apply(lambda x: x.split('_')[2])
+    results_df = results_df[['model','seed', 'lost_job_1mo', 'is_hired_1mo', 'is_unemployed', 'job_offer', 'job_search']]
+    results_df.to_csv(os.path.join(results_folder, 'auc_results.csv'))
+    print(results_df.head())
+    #logger.info(results_dict)
+
+
+    # logger.info(results_folders_list)
+    # job_ids_list = [x.split('_')[1] for x in results_folders_list]
+    # logger.info(job_ids_list)
+    # # keep 10 latest jobs
+    # indexes_to_keep_list = sorted(range(len(job_ids_list)), key=lambda x: job_ids_list[x])[-15:]
+    # logger.info(indexes_to_keep_list)
+    # results_dict = dict()
+    # model_types_list = list()
+    # for index in indexes_to_keep_list:
+    #     result_folder_name_str = results_folders_list[index]
+    #     print(result_folder_name_str)
+    #     model_type_str = f"{result_folder_name_str.split('_')[0]}_{result_folder_name_str.split('_')[2]}"
+    #     if model_type_str not in model_types_list:
+    #         model_types_list.append(model_type_str)
+    #     if model_type_str not in results_dict.keys():
+    #         results_dict[model_type_str] = dict()
+    #     # results_dict[model_type_str] = dict()
+    #     for label in ['lost_job_1mo', 'is_hired_1mo', 'is_unemployed', 'job_offer', 'job_search']:
+    #         path_data = os.path.join(args.results_folder, result_folder_name_str, f'val_{label}_evaluation.csv')
+    #         if Path(path_data).exists():
+    #             df = pd.read_csv(os.path.join(args.results_folder, result_folder_name_str, f'holdout_{label}_evaluation.csv'),
+    #                              index_col=0)
+    #             results_dict[model_type_str][f'auc_{label}'] = float(df['value']['auc'])
+    # # get average AUC for each label and model type
+    # # if len(model_types_list) != 2:
+    # #     logger.error("More than two models are being compared.")
+    # logger.info(model_types_list)
+    # logger.info(results_dict)
+    # #average_auc_model_1_dict = build_auc_dict(results_dict=results_dict, model_type=model_types_list[0])
+    # results_df = pd.DataFrame.from_dict(results_dict, orient='index')
+    # results_df = results_df.round(3)
+    # print(results_df.head(n=15))
+    # # if not os.path.exists(output_path):
+    # #     os.makedirs(output_path)
+    # # results_df.to_csv(os.path.join(output_path, f'{args.run_name}.csv'))
 
