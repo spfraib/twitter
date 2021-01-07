02c8b0cb120918b4493eb0ce6245e98905e823ca
Manuel Tonneau <manuel.tonneau@mailbox.org>
2021-01-07 11:53:18 +0100

    Modify convert to convert best models iter0

--- code/2-twitter_labor/4-inference_200M/bert_models/onnx_model_conversion_2021.py
+++ code/2-twitter_labor/4-inference_200M/bert_models/onnx_model_conversion_2021.py
@@ -12,18 +12,36 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+def convert_model_path_to_model_name(model_path):
+    if 'bertweet' in model_path:
+        return 'vinai/bertweet-base'
+    elif 'roberta-base' in model_path:
+        return 'roberta-base'
+    elif 'DeepPavlov' in model_path:
+        return 'DeepPavlov/bert-base-cased-conversational'
+
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--model_path", help="path to pytorch models (with onnx model in model_path/onnx/")
-parser.add_argument("--model_name", help="Name of model in HF model hub")
+parser.add_argument("--iter", type=int, help="path to pytorch models (with onnx model in model_path/onnx/")
+parser.add_argument("--country_code", type=str)
 
 args = parser.parse_args()
+
+best_model_paths_dict = {
+    'iter0': {
+        'US': {
+            'lost_job_1mo': 'vinai_bertweet-base_jan5_iter0_928517_SEED_7',
+            'is_hired_1mo': 'vinai_bertweet-base_jan5_iter0_928525_SEED_15',
+            'is_unemployed': 'vinai_bertweet-base_jan5_iter0_928513_SEED_3',
+            'job_offer': 'roberta-base_jan5_iter0_928467_SEED_2',
+            'job_search': 'vinai_bertweet-base_jan5_iter0_928513_SEED_3'
+        }}}
 
 for label in ["lost_job_1mo", "is_unemployed", "job_search", "is_hired_1mo", "job_offer"]:
 
     logger.info(f'*****************************{label}*****************************')
-    model_path = args.model_path.format(label)
-    onnx_path = model_path + '/onnx'.format(label)
+    model_path = f'/scratch/mt4493/twitter_labor/trained_models/{args.country_code}/{best_model_paths_dict[args.iter][args.country_code][label]}/{label}/models/best_model'
+    onnx_path = os.path.join(model_path, 'onnx')
 
     try:
         shutil.rmtree(onnx_path)  # deleting onxx folder and contents, if exists, conversion excepts
@@ -34,7 +52,7 @@ for label in ["lost_job_1mo", "is_unemployed", "job_search", "is_hired_1mo", "jo
     logger.info('>> converting..')
     convert(framework="pt",
             model=model_path,
-            tokenizer=args.model_name,
+            tokenizer=convert_model_path_to_model_name(model_path),
             output=Path(os.path.join(onnx_path, 'converted.onnx')),
             opset=11,
             pipeline_name='sentiment-analysis')
