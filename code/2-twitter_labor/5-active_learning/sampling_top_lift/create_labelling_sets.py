0514b1fae30b747ca325166fbb00df77dc57910b
Manuel Tonneau <manuel.tonneau@mailbox.org>
2021-02-04 17:57:55 +0100

    Add batch file to create labelling sets

--- code/2-twitter_labor/5-active_learning/sampling_top_lift/create_labelling_sets.py
+++ code/2-twitter_labor/5-active_learning/sampling_top_lift/create_labelling_sets.py
@@ -3,6 +3,12 @@ from pathlib import Path
 import numpy as np
 import os
 import argparse
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def get_args_from_command_line():
     """Parse the command line arguments."""
@@ -21,7 +27,9 @@ def turn_token_array_to_string(x):
 
 if __name__ == '__main__':
     args = get_args_from_command_line()
-
+    output_path = f'/scratch/mt4493/twitter_labor/twitter-labor-data/data/active_learning/sampling_top_lift/{args.country_code}/{args.inference_folder}/labeling_sample'
+    if not os.exists(output_path):
+        os.makedirs(output_path)
     for label in ['is_hired_1mo', 'is_unemployed', 'job_search', 'lost_job_1mo']:
         for ngram in ['two_grams', 'three_grams']:
             ngram_path = f'/scratch/mt4493/twitter_labor/twitter-labor-data/data/active_learning/sampling_top_lift/{args.country_code}/{args.inference_folder}/{label}/{ngram}'
@@ -30,10 +38,10 @@ if __name__ == '__main__':
             df['tokens_str'] = df['tokens'].apply(turn_token_array_to_string)
             df_sample = df.groupby('tokens_str', group_keys=False).apply(lambda df: df.sample(5)).reset_index(drop=True)
             df_sample = df_sample[['tweet_id', 'text', 'tokens_str']]
-            output_path = f'/scratch/mt4493/twitter_labor/twitter-labor-data/data/active_learning/sampling_top_lift/{args.country_code}/{args.inference_folder}/labeling_sample'
             df_sample.to_parquet(os.path.join(output_path, f'sample_{label}_{ngram}.parquet'), index=False)
-
+            logging.info(f'Sample for label {label} {ngram} saved at: {output_path}/sample_{label}_{ngram}.parquet')
     labeling_path = f'/scratch/mt4493/twitter_labor/twitter-labor-data/data/ngram_samples/{args.country_code}/iter{args.iter_number}/labeling'
     list_sample_parquet = Path(output_path).glob('*.parquet')
     df = pd.concat(map(pd.read_parquet, list_sample_parquet)).reset_index(drop=True)
     df.to_parquet(os.path.join(labeling_path, 'merged.parquet'))
+    logging.info(f'Whole labeling set saved at {labeling_path}/merged.parquet')
