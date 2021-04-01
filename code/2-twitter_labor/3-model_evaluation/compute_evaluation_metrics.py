6b33936909355038413fd2b1cf121ca21fe80974
Manuel Tonneau <manuel.tonneau@mailbox.org>
2021-04-01 15:28:07 +0200

    Modify expansion rate calculation

--- code/2-twitter_labor/3-model_evaluation/compute_evaluation_metrics.py
+++ code/2-twitter_labor/3-model_evaluation/compute_evaluation_metrics.py
@@ -31,23 +31,23 @@ if __name__ == '__main__':
     random_path = f'{data_path}/random_samples/random_samples_splitted'
     random_path_new_samples = Path(os.path.join(random_path, args.country_code, 'evaluation'))
     # preliminary data
-    tweet_count_seedlist_dict = {
-        'US': {
-            'lost_job_1mo': 6749,
-            'is_hired_1mo': 17818,
-            'is_unemployed': 20831,
-            'job_search': 16499,
-            'job_offer': 512267}}
-    expansion_rate_denom = sum(tweet_count_seedlist_dict[args.country_code].values())
+    # tweet_count_seedlist_dict = {
+    #     'US': {
+    #         'lost_job_1mo': 6749,
+    #         'is_hired_1mo': 17818,
+    #         'is_unemployed': 20831,
+    #         'job_search': 16499,
+    #         'job_offer': 512267}}
+    # expansion_rate_denom = sum(tweet_count_seedlist_dict[args.country_code].values())
     inference_folder_dict = {
         'US': ['iter_0-convbert-969622-evaluation', 'iter_1-convbert-3050798-evaluation',
                'iter_2-convbert-3134867-evaluation', 'iter_3-convbert-3174249-evaluation',
                'iter_4-convbert-3297962-evaluation']}
-    diversity_model_dict = {
-        'US': 'stsb-roberta-large',
-        'MX': 'distiluse-base-multilingual-cased-v2',
-        'BR': 'distiluse-base-multilingual-cased-v2'}
-    diversity_model = SentenceTransformer(diversity_model_dict[args.country_code])
+    # diversity_model_dict = {
+    #     'US': 'stsb-roberta-large',
+    #     'MX': 'distiluse-base-multilingual-cased-v2',
+    #     'BR': 'distiluse-base-multilingual-cased-v2'}
+    # diversity_model = SentenceTransformer(diversity_model_dict[args.country_code])
 
     # load random set
     random_df = pd.concat(
@@ -55,6 +55,11 @@ if __name__ == '__main__':
         for parquet_file in random_path_new_samples.glob('*.parquet')
     )
     logger.info('Loaded random data')
+    # load seedlist keyword indicator
+    seedlist_path = f'/scratch/mt4493/twitter_labor/twitter-labor-data/data/random_samples/random_samples_splitted/{args.country_code}/evaluation_seedlist_keyword'
+    seedlist_df = pd.read_parquet(os.path.join(seedlist_path, 'evaluation_seedlist_keyword.parquet'))
+    random_df =  random_df.merge(seedlist_df, on="tweet_id", how='inner')
+
     inference_path = os.path.join(data_path, 'inference')
     results_dict = dict()
     to_label_list = list()
@@ -76,23 +81,24 @@ if __name__ == '__main__':
             top_df['tweet_type'] = 'top_50'
             # restrict to score > T
             all_df = all_df.loc[all_df['score'] > args.threshold].reset_index(drop=True)
+            all_seedlist_df = all_df.loc[all_df['seedlist_keyword'] == 1]
             all_df['inference_folder'] = inference_folder
             logger.info(f'# tweets with score > {args.threshold}: {all_df.shape[0]}')
             # compute and save expansion rate
             results_dict[inference_folder][label] = dict()
-            expansion_rate = all_df.shape[0] / expansion_rate_denom
+            expansion_rate = all_seedlist_df.shape[0] / all_df.shape[0]
             logger.info(f'Expansion rate: {expansion_rate}')
             results_dict[inference_folder][label]['expansion_rate'] = expansion_rate
             # compute and save diversity score
-            if all_df.shape[0] > 0:
-                tweet_list = all_df['text'].tolist()
-                embeddings = diversity_model.encode(tweet_list, convert_to_tensor=True)
-                cosine_scores = util.pytorch_cos_sim(embeddings, embeddings)
-                diversity_score = (-torch.sum(cosine_scores) / (len(tweet_list) ** 2)).item()
-                logger.info(f'Diversity score: {diversity_score}')
-                results_dict[inference_folder][label]['diversity_score'] = diversity_score
-            else:
-                results_dict[inference_folder][label]['diversity_score'] = np.nan
+            # if all_df.shape[0] > 0:
+            #     tweet_list = all_df['text'].tolist()
+            #     embeddings = diversity_model.encode(tweet_list, convert_to_tensor=True)
+            #     cosine_scores = util.pytorch_cos_sim(embeddings, embeddings)
+            #     diversity_score = (-torch.sum(cosine_scores) / (len(tweet_list) ** 2)).item()
+            #     logger.info(f'Diversity score: {diversity_score}')
+            #     results_dict[inference_folder][label]['diversity_score'] = diversity_score
+            # else:
+            #     results_dict[inference_folder][label]['diversity_score'] = np.nan
             # save tweets to label for precision estimate
             if all_df.shape[0] > 0:
                 sample_df = all_df.sample(n=50).reset_index(drop=True)
@@ -112,7 +118,7 @@ if __name__ == '__main__':
     results_df = pd.concat(results_list)
     # save results
     output_path = f'{data_path}/evaluation_metrics/{args.country_code}/threshold_{int(args.threshold*100)}'
-    results_df.to_csv(os.path.join(output_path, 'expansion_diversity.csv'))
+    results_df.to_csv(os.path.join(output_path, 'expansion.csv'))
     # appended_to_label_df = pd.concat(to_label_list)
     # output_path = f'{data_path}/evaluation/{args.country_code}/{args.inference_folder}'
     # if not os.path.exists(output_path):
