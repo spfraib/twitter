b9cd8b552a648fed997dc8fecdedf99c50447c67
Manuel Tonneau <manuel.tonneau@mailbox.org>
2021-04-22 20:45:55 +0200

    Add formatting tweet_id

--- code/2-twitter_labor/5-active_learning/adaptive_retrieval/retrieve_top_tweets.py
+++ code/2-twitter_labor/5-active_learning/adaptive_retrieval/retrieve_top_tweets.py
@@ -44,6 +44,7 @@ if __name__ == '__main__':
         pd.read_parquet(parquet_file)
         for parquet_file in random_path_new_samples.glob('*.parquet')
     )
+    random_df['tweet_id'] = random_df['tweet_id'].apply(lambda x: str(int(float(x))))
     print('Loaded random data')
     print('Shape random data', random_df.shape)
     raw_labels_path_dict = {'US': {0: 'jan5_iter0',
