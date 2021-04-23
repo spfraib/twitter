4d791cb561a5ac321ac8a91268e3a97c8a083703
dval <nuclearr.d@gmail.com>
2021-04-22 21:09:39 -0400

    percussive debugging

--- code/2-twitter_labor/4-inference_200M/bert_models/replicator_NYU.py
+++ code/2-twitter_labor/4-inference_200M/bert_models/replicator_NYU.py
@@ -6,7 +6,7 @@ for model_type in ['converted.onnx', 'converted-optimized.onnx', 'converted-opti
         for replication in range(5):
             print(model_type, batchsize, replication)
             os.system(
-                'python inference_ONNX_bert_100M_random_dhaval_optimized_batch_compared_torch_and_onnx.py --input_path '
+                'python inference_ONNX_bert_100M_random_dhaval_optimized_batch_compared_torch_and_onnx_NYU.py --input_path '
                 '/scratch/mt4493/twitter_labor/twitter-labor-data/data/random_samples/random_samples_splitted/US/test --output_path '
                 '/scratch/mt4493/twitter_labor/code/twitter/code/2-twitter_labor/4-inference_200M/bert_models/temp_output_speedtest --country_code US --iteration_number 2 '
                 '--batchsize {} '
