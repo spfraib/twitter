d1a95d2fa97a8eb79417e66d23afb3f702a02db1
Manuel Tonneau <manuel.tonneau@mailbox.org>
2021-05-01 12:10:44 +0200

    Push latest codes

--- code/2-twitter_labor/5-active_learning/uncertainty_sampling/retrieve_tweets_around_threshold.py
+++ code/2-twitter_labor/5-active_learning/uncertainty_sampling/retrieve_tweets_around_threshold.py
@@ -2,15 +2,16 @@ import pandas as pd
 import argparse
 import os
 from pathlib import Path
-import numpy as np 
-import pickle 
+import numpy as np
+import pickle
 from scipy import optimize
-import logging 
+import logging
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 def get_args_from_command_line():
     """Parse the command line arguments."""
@@ -29,24 +30,27 @@ def discard_already_labelled_tweets(path_to_labelled, to_label_df):
     parquet_file_list = list(Path(path_to_labelled).glob('*.parquet'))
     if len(parquet_file_list) > 0:
         df = pd.read_parquet(os.path.join(path_to_labelled, 'all_labels_with_text.parquet'))
-        logger.info(f'Shape old labels: {df.shape[0]}' )
+        logger.info(f'Shape old labels: {df.shape[0]}')
         df = df[['tweet_id']]
         # df['tweet_id'] = df['tweet_id'].apply(lambda x: str(int(float(x))))
         df = df.drop_duplicates().reset_index(drop=True)
-        logger.info(f'Shape old labels (after dropping duplicates): {df.shape[0]}' )
+        logger.info(f'Shape old labels (after dropping duplicates): {df.shape[0]}')
         list_labelled_tweet_ids = df['tweet_id'].tolist()
         to_label_df = to_label_df[~to_label_df['tweet_id'].isin(list_labelled_tweet_ids)].reset_index(drop=True)
         return to_label_df
     else:
         return to_label_df
 
+
 def func(x, params):
     all_calibrated_scores = [1 / (1 + np.exp(-(param[0] * x + param[1]))) for param in params]
     return np.mean(all_calibrated_scores, axis=0) - 0.5
 
+
 def calibrate(x, params):
     all_calibrated_scores = [1 / (1 + np.exp(-(param[0] * x + param[1]))) for param in params]
     return np.mean(all_calibrated_scores, axis=0)
+
 
 if __name__ == '__main__':
     args = get_args_from_command_line()
@@ -60,16 +64,26 @@ if __name__ == '__main__':
     # random_df['tweet_id'] = random_df['tweet_id'].apply(lambda x: str(int(float(x))))
     logger.info('Loaded random data')
     logger.info(f'Shape random data: {random_df.shape[0]}')
-    raw_labels_path_dict = {'US': {0: 'jan5_iter0',
-                              1: 'apr30_iter1_uncertainty',
-                              2: 'apr22_iter2_adaptive',
-                              3: 'feb25_iter3'},
-                            'MX': {0: 'feb27_iter0', 1: 'mar12_iter1', 2: 'mar23_iter2', 3: 'mar30_iter3'},
-                            'BR': {0: 'feb16_iter0', 1: 'mar12_iter1', 2: 'mar24_iter2', 3: 'apr1_iter3'}}
+    folder_dict = {
+        0: {
+            'inference_eval': 'iter_0-convbert-969622-evaluation',
+            'data_folder': 'jan5_iter0'},
+        1: {
+            'inference_eval': 'iter_1-convbert_uncertainty-6200469-evaluation',
+            'data_folder': 'apr30_iter1_uncertainty'},
+        2: {
+            'inference_eval': '',
+            'data_folder': ''},
+        3: {
+            'inference_eval': '',
+            'data_folder': ''},
+        4: {
+            'inference_eval': '',
+            'data_folder': ''}}
     for iteration_number in range(int(args.iteration_number)):
         logger.info(f'Iteration {iteration_number}')
         random_count = random_df.shape[0]
-        data_folder_name = raw_labels_path_dict[args.country_code][iteration_number]
+        data_folder_name = folder_dict[iteration_number]['data_folder']
         path_to_labelled = f'/scratch/mt4493/twitter_labor/twitter-labor-data/data/train_test/{args.country_code}/{data_folder_name}/raw'
         random_df = discard_already_labelled_tweets(
             path_to_labelled=path_to_labelled,
@@ -77,39 +91,25 @@ if __name__ == '__main__':
         # random_df = discard_already_labelled_tweets_csv(
         #     path_to_labelled=path_to_labelled,
         #     to_label_df=random_df)
-        logger.info(f'Dropped {str(random_count - random_df.shape[0])} tweets already labelled at iteration {iteration_number}')
-
-    folder_dict = {
-        0: [{
-            'eval': 'iter_0-convbert-969622-evaluation',
-            'new_samples': 'iter_0-convbert-1122153-new_samples'}, 'jan5_iter0'],
-        1: [{
-            'eval': 'iter_1-convbert_uncertainty-6200469-evaluation',
-            'new_samples': 'iter_1-convbert-3062566-new_samples'}, 'feb22_iter1'],
-        2: [{
-            'eval': 'iter_2-convbert-3134867-evaluation',
-            'new_samples': 'iter_2-convbert-3139138-new_samples'}, 'feb23_iter2'],
-        3: [{
-            'eval': 'iter_3-convbert-3174249-evaluation',
-            'new_samples': 'iter_3-convbert-3178321-new_samples'}, 'feb25_iter3'],
-        4: [{
-            'eval': 'iter_4-convbert-3297962-evaluation',
-            'new_samples': 'iter_4-convbert-3308838-new_samples'}, 'mar1_iter4']}
+        logger.info(
+            f'Dropped {str(random_count - random_df.shape[0])} tweets already labelled at iteration {iteration_number}')
 
     path_to_params = '/scratch/mt4493/twitter_labor/code/twitter/code/2-twitter_labor/3-model_evaluation/expansion/preliminary/calibration_dicts'
-    params_dict = pickle.load(open(os.path.join(path_to_params, f'calibration_dict_uncertainty_10000_iter{args.iteration_number - 1}.pkl'), 'rb'))
-    inference_folder = folder_dict[args.iteration_number - 1][0]['eval']
-    
+    params_dict = pickle.load(
+        open(os.path.join(path_to_params, f'calibration_dict_uncertainty_10000_iter{int(args.iteration_number) - 1}.pkl'),
+             'rb'))
+    inference_eval_folder = folder_dict[args.iteration_number - 1]['inference_eval']
+
     sample_df_list = list()
     for label in ['is_hired_1mo', 'lost_job_1mo', 'job_search', 'is_unemployed', 'job_offer']:
         logger.info(f'Label: {label}')
         # find root
-        params = params_dict[label][inference_folder]['params']
+        params = params_dict[label][inference_eval_folder]['params']
         root = optimize.brentq(func, 0, 1, args=(params))
         logger.info(f'Root: {root}')
         logger.info(f'Calibrated score for root: {calibrate(root, params)}')
         # Load scores
-        inference_path = os.path.join(data_path,'inference')
+        inference_path = os.path.join(data_path, 'inference')
         scores_path = Path(os.path.join(inference_path, args.country_code, args.inference_folder, 'output', label))
         scores_df = pd.concat(
             pd.read_parquet(parquet_file)
@@ -131,11 +131,6 @@ if __name__ == '__main__':
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     appended_sample_df.to_parquet(os.path.join(output_path, 'top_tweets.parquet'), index=False)
-
-
-
-
-
 
 # for label in ['is_hired_1mo', 'lost_job_1mo', 'is_unemployed', 'job_search', 'job_offer']:
 #     path_to_scores = os.path.join('/user/mt4493/twitter/twitter-labor-data/inference', country_code, inference_folder, 'calibrated_output', label)  # Prediction scores from classification
