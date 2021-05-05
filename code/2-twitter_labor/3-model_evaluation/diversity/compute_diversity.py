577a2a9c1d2a0a1f6687aa32a01db1b2e2d3741f
Manuel Tonneau <manuel.tonneau@mailbox.org>
2021-05-05 11:06:23 +0200

    Parallelize code

--- code/2-twitter_labor/3-model_evaluation/diversity/compute_diversity.py
+++ code/2-twitter_labor/3-model_evaluation/diversity/compute_diversity.py
@@ -7,6 +7,8 @@ from sentence_transformers import SentenceTransformer, util
 import torch
 import numpy as np
 import re
+import itertools
+import json
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
@@ -22,9 +24,7 @@ def get_args_from_command_line():
     parser.add_argument("--method", type=str, default='topk')
     parser.add_argument("--threshold", type=float,
                         default=0.95)
-    parser.add_argument("--topk", type=int)
-    parser.add_argument("--active_learning", type=str, default='our_method')
-
+    parser.add_argument("--topk", type=int, default=10000)
     args = parser.parse_args()
     return args
 
@@ -37,22 +37,54 @@ def compute_mean_max_diversity(matrix):
     return np.average(max_list)
 
 
+def get_env_var(varname, default):
+    if os.environ.get(varname) != None:
+        var = int(os.environ.get(varname))
+        print(varname, ':', var)
+    else:
+        var = default
+        print(varname, ':', var, '(Default)')
+    return var
+
+
 if __name__ == '__main__':
     args = get_args_from_command_line()
+    # Load env vars
+    SLURM_ARRAY_TASK_ID = get_env_var('SLURM_ARRAY_TASK_ID', 0)
+    SLURM_ARRAY_TASK_COUNT = get_env_var('SLURM_ARRAY_TASK_COUNT', 1)
+    SLURM_JOB_ID = get_env_var('SLURM_JOB_ID', 1)
     # define paths
     data_path = '/scratch/mt4493/twitter_labor/twitter-labor-data/data'
     random_path = f'{data_path}/random_samples/random_samples_splitted'
     random_path_new_samples = Path(os.path.join(random_path, args.country_code, 'evaluation'))
 
     inference_folder_dict = {
-        'US': {
-            'our_method': ['iter_0-convbert-969622-evaluation', 'iter_1-convbert-3050798-evaluation',
-                           'iter_2-convbert-3134867-evaluation', 'iter_3-convbert-3174249-evaluation',
-                           'iter_4-convbert-3297962-evaluation'],
-            'adaptive': ['iter_0-convbert-969622-evaluation', 'iter_1-convbert_adaptive-5612019-evaluation',
-                         'iter_2-convbert_adaptive-5972342-evaluation', 'iter_3-convbert_adaptive-5998181-evaluation',
-                         'iter_4-convbert_adaptive-6057405-evaluation']
-            }}
+        'our_method': {
+            0: 'iter_0-convbert-969622-evaluation',
+            1: 'iter_1-convbert-3050798-evaluation',
+            2: 'iter_2-convbert-3134867-evaluation',
+            3: 'iter_3-convbert-3174249-evaluation',
+            4: 'iter_4-convbert-3297962-evaluation'},
+        'adaptive': {
+            1: 'iter_1-convbert_adaptive-5612019-evaluation',
+            2: 'iter_2-convbert_adaptive-5972342-evaluation',
+            3: 'iter_3-convbert_adaptive-5998181-evaluation',
+            4: 'iter_4-convbert_adaptive-6057405-evaluation'},
+        'uncertainty': {
+            1: 'iter_1-convbert_uncertainty-6200469-evaluation',
+            2: 'iter_2-convbert_uncertainty-6253253-evaluation',
+            3: 'iter_3-convbert_uncertainty-6318280-evaluation'}}
+    # Define and select combination
+    labels = ['job_search', 'job_offer', 'is_hired_1mo', 'lost_job_1mo', 'is_unemployed']
+    combinations_list = list(itertools.product(*[['our_method', 'adaptive', 'uncertainty'], range(5), labels]))
+    combinations_list = [combination for combination in combinations_list if
+                         combination[:2] not in [('adaptive', 0), ('uncertainty', 0), ('uncertainty', 4)]]
+    selected_combinations = list(np.array_split(
+        combinations_list,
+        SLURM_ARRAY_TASK_COUNT)[SLURM_ARRAY_TASK_ID])
+    logger.info(f'Selected combinations: {selected_combinations}')
+
+    # load model
     diversity_model_dict = {
         'US': 'stsb-roberta-large',
         'MX': 'distiluse-base-multilingual-cased-v2',
@@ -69,73 +101,54 @@ if __name__ == '__main__':
     inference_path = os.path.join(data_path, 'inference')
     results_dict = dict()
     to_label_list = list()
-    for inference_folder in inference_folder_dict[args.country_code][args.active_learning]:
+    for combination in selected_combinations:
+        inference_folder = inference_folder[combination[0]][combination[1]]
         logger.info(f'**** Inference folder: {inference_folder} ****')
         results_dict[inference_folder] = dict()
-        for label in ['is_hired_1mo', 'lost_job_1mo', 'job_search', 'is_unemployed', 'job_offer']:
-            logger.info(f'** Class: {label} **')
-            scores_path = Path(os.path.join(inference_path, args.country_code, inference_folder, 'output', label))
-            scores_df = pd.concat(
-                pd.read_parquet(parquet_file)
-                for parquet_file in scores_path.glob('*.parquet')
-            )
-            logger.info('Loaded scores')
-            all_df = scores_df.merge(random_df, on="tweet_id", how='inner')
-            all_df = all_df.sort_values(by=['score'], ascending=False).reset_index(drop=True)
-            # # keep aside 50 top tweets
-            # top_df = all_df[:50]
-            # top_df['tweet_type'] = 'top_50'
-            # restrict to score > T
-            if args.method == 'threshold':
-                all_df = all_df.loc[all_df['score'] > args.threshold].reset_index(drop=True)
-            elif args.method == 'topk':
-                all_df = all_df[:args.topk]
-            all_df['inference_folder'] = inference_folder
+        label = combination[2]
+        logger.info(f'** Class: {label} **')
+        scores_path = Path(os.path.join(inference_path, args.country_code, inference_folder, 'output', label))
+        scores_df = pd.concat(
+            pd.read_parquet(parquet_file)
+            for parquet_file in scores_path.glob('*.parquet')
+        )
+        logger.info('Loaded scores')
+        all_df = scores_df.merge(random_df, on="tweet_id", how='inner')
+        all_df = all_df.sort_values(by=['score'], ascending=False).reset_index(drop=True)
+        # # keep aside 50 top tweets
+        # top_df = all_df[:50]
+        # top_df['tweet_type'] = 'top_50'
+        # restrict to score > T
+        if args.method == 'threshold':
+            all_df = all_df.loc[all_df['score'] > args.threshold].reset_index(drop=True)
             logger.info(f'# tweets with score > {args.threshold}: {all_df.shape[0]}')
+        elif args.method == 'topk':
+            all_df = all_df[:args.topk]
+        all_df['inference_folder'] = inference_folder
 
-            results_dict[inference_folder][label] = dict()
-            # compute and save diversity score
-            if all_df.shape[0] > 0:
-                tweet_list = all_df['text'].tolist()
-                embeddings = diversity_model.encode(tweet_list, convert_to_tensor=True)
-                cosine_scores = util.pytorch_cos_sim(embeddings, embeddings)
-                diversity_score = compute_mean_max_diversity(matrix=cosine_scores)
-                # diversity_score = (-torch.sum(cosine_scores) / (len(tweet_list) ** 2)).item()
-                logger.info(f'Diversity score: {diversity_score}')
-                results_dict[inference_folder][label]['diversity_score'] = diversity_score
-            else:
-                results_dict[inference_folder][label]['diversity_score'] = np.nan
-            # save tweets to label for precision estimate
-            # if all_df.shape[0] > 0:
-            #     sample_df = all_df.sample(n=50).reset_index(drop=True)
-            #     sample_df['tweet_type'] = 'sample_50'
-            #     to_label_df = pd.concat([top_df, sample_df]).reset_index(drop=True)
-            #     to_label_list.append(to_label_df)
-            # else:
-            #     logger.info('Only sending top 50 tweets to labeling')
-            #     to_label_list.append(top_df)
-    # organize results
-    results_df = pd.DataFrame.from_dict(results_dict)
-    results_list = list()
-    for inference_folder in inference_folder_dict[args.country_code]:
-        results_iter_df = results_df[inference_folder].apply(pd.Series)
-        iter_number = int(re.findall('iter_(\d)', inference_folder)[0])
-        results_iter_df['iter'] = iter_number
-        results_list.append(results_iter_df)
-    results_df = pd.concat(results_list)
+        results_dict[inference_folder][label] = dict()
+        # compute and save diversity score
+        if all_df.shape[0] > 0:
+            tweet_list = all_df['text'].tolist()
+            embeddings = diversity_model.encode(tweet_list, convert_to_tensor=True)
+            logger.info('Converted tweets to embeddings')
+            cosine_scores = util.pytorch_cos_sim(embeddings, embeddings)
+            logger.info('Computed cosine similarity')
+            diversity_score = compute_mean_max_diversity(matrix=cosine_scores)
+            # diversity_score = (-torch.sum(cosine_scores) / (len(tweet_list) ** 2)).item()
+            logger.info(f'Diversity score: {diversity_score}')
+            results_dict[inference_folder][label]['diversity_score'] = diversity_score
+        else:
+            results_dict[inference_folder][label]['diversity_score'] = np.nan
     # save results
     if args.method == 'threshold':
         folder_name = f'threshold_{int(args.threshold * 100)}'
     elif args.method == 'topk':
         folder_name = f'top_{args.topk}'
-    output_path = f'{data_path}/evaluation_metrics/{args.country_code}/{folder_name}/{args.active_learning}'
+    output_path = f'{data_path}/evaluation_metrics/{args.country_code}/diversity/{folder_name}
     if not os.path.exists(output_path):
         os.makedirs(output_path)
-    results_df = results_df.reset_index()
-    results_df = results_df.sort_values(by=['index', 'iter']).reset_index(drop=True)
-    results_df.to_csv(os.path.join(output_path, 'diversity_random_set.csv'), index=False)
-    # appended_to_label_df = pd.concat(to_label_list)
-    # output_path = f'{data_path}/evaluation/{args.country_code}/{args.inference_folder}'
-    # if not os.path.exists(output_path):
-    #     os.makedirs(output_path)
-    # appended_to_label_df.to_parquet(os.path.join(output_path, 'top_tweets.parquet'), index=False)
+    # save results dict
+    with open(os.path.join(output_path, f'diversity_{str(SLURM_ARRAY_TASK_ID)}.json'), 'w') as f:
+        json.dump(results_dict, f)
+
