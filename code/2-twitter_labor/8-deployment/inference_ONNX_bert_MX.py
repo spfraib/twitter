3b0cbda9bc935fccbb3adbb8dcb7e9311aac6749
dval <nuclearr.d@gmail.com>
2021-06-21 20:02:27 -0400

    [bug fix]

--- code/2-twitter_labor/8-deployment/inference_ONNX_bert_MX.py
+++ code/2-twitter_labor/8-deployment/inference_ONNX_bert_MX.py
@@ -159,29 +159,38 @@ start_time = time.time()
 # final_output_path = args.output_path #e.g. /scratch/spf248/twitter/data/user_timeline/bert_inferrred/MX
 final_output_path = '/scratch/spf248/twitter/data/user_timeline/user_timeline_BERT_scores_labor/MX/iter_6-mexrun_5GB' \
                     '-7309008/output/is_hired_1mo' #DEBUG
+
 if not os.path.exists(os.path.join(final_output_path)):
     print('>>>> directory doesnt exists, creating it')
     os.makedirs(os.path.join(final_output_path))
 
 input_files_list = glob(os.path.join(path_to_data, '*.parquet'))
 
+"""
+creating a list of unique file ids assuming this file name structure:
+/scratch/spf248/twitter/data/user_timeline/user_timeline_text_preprocessed/part-00000-52fdb0a4-e509-49fe-9f3a-d809594bba7d-c000.snappy.parquet
+in this case:
+unique_intput_file_id_list will contain 00000-52fdb0a4-e509-49fe-9f3a-d809594bba7d
+filename_prefix is /scratch/spf248/twitter/data/user_timeline/user_timeline_text_preprocessed/part-
+filename_suffix is -c000.snappy.parquet
+"""
 unique_intput_file_id_list = [filename.split('part-')[1].split('-c000')[0]
                               for filename in input_files_list]
 filename_prefix = input_files_list[0].split('part-')[0]
 filename_suffix = input_files_list[0].split('part-')[1].split('-c000')[1]
 
 
-print(unique_intput_file_id_list[:10], filename_prefix, filename_suffix)
+print(input_files_list[:4], filename_prefix, filename_suffix)
 
 print('^^^^^^^', path_to_data, final_output_path, args.resume)
 already_processed_output_files = glob(os.path.join(final_output_path, '*.parquet'))
 unique_already_processed_file_id_list = [filename.split('part-')[1].split('-c000')[0]
                               for filename in already_processed_output_files]
 
-print('already_processed_output_files', unique_already_processed_file_id_list[:10])
+print('already_processed_output_files', unique_already_processed_file_id_list[:5])
 if args.resume == 1:
     unique_ids_remaining = list(set(unique_intput_file_id_list) - set(unique_already_processed_file_id_list))
-    files_remaining = [filename_prefix+filename+filename_suffix for filename in unique_ids_remaining]
+    files_remaining = [filename_prefix+'part-'+filename+'-c000'+filename_suffix for filename in unique_ids_remaining]
     print(files_remaining[:3])
     print(len(files_remaining), len(unique_intput_file_id_list), len(unique_already_processed_file_id_list))
     print('true')
