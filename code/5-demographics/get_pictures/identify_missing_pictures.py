e72f2908f2557ff00b871b3cde35e361418530fa
Manuel <manuel.tonneau@mailbox.org>
2021-10-01 16:24:42 +0200

    Add info about errors

--- code/5-demographics/get_pictures/identify_missing_pictures.py
+++ code/5-demographics/get_pictures/identify_missing_pictures.py
@@ -28,6 +28,18 @@ if __name__ == '__main__':
     user_data_path = f'{data_path}/user_timeline/user_timeline_crawled/{args.country_code}'
     user_df = pd.concat([pd.read_parquet(parquet_path) for parquet_path in Path(user_data_path).glob('*.parquet')])
     user_list = user_df['user_id'].unique().tolist()
+    logger.info(f'Total # of users: {len(user_list)}')
+    # get ids from user for whom we got an error when trying to download their pictures
+    list_errors_path = f'{data_path}/demographics/profile_pictures/tars/list_errors_{args.country_code}.txt.gz'
+    user_id_errors_list = list()
+    with gzip.open(list_errors_path, 'rt') as f:
+        for line in f:
+            line = line.replace('\n', '')
+            if line.isdigit():
+                user_id_errors_list.append(line)
+            else:
+                logger.info(f'User ID {line} from erroneous users is not a digit')
+    logger.info(f'# of users whose picture we were not able to download: {len(user_id_errors_list)}')
     # get ids from users with downloaded pictures
     list_files_path = f'{data_path}/demographics/profile_pictures/tars/list_files_{args.country_code}.txt.gz'
     user_with_pictures_list = list()
@@ -49,8 +61,8 @@ if __name__ == '__main__':
                 user_with_pictures_list.append(user_id_str)
     logger.info(f'File format repartition: {file_format_count_dict}')
     counter = Counter(user_with_pictures_list)
-    logger.info(f'Total # of users: {len(user_list)}')
     logger.info(f'# unique user IDs with pictures: {len(list(counter))}')
     logger.info(f'# user IDs with more than one picture: {len([i for i in counter if counter[i]>1])}')
-    users_without_pictures_list = [user_id for user_id in user_list if user_id not in user_with_pictures_list]
+    pictures_and_error_list = user_with_pictures_list + user_id_errors_list
+    users_without_pictures_list = [user_id for user_id in user_list if user_id not in pictures_and_error_list ]
     logger.info(f'# users without picture: {len(users_without_pictures_list)}')
