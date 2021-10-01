5a1bba3d9be31ff2699e54601ed8a465a8f5a77a
Manuel <manuel.tonneau@mailbox.org>
2021-10-01 11:12:16 +0200

    Correct output path

--- code/5-demographics/get_pictures/get_filenames_from_tars.py
+++ code/5-demographics/get_pictures/get_filenames_from_tars.py
@@ -24,10 +24,10 @@ if __name__ == '__main__':
     args = get_args_from_command_line()
     tar_folder_dict = {'normal': 'tars', 'resized': 'resized_tars'}
     data_path = f'/scratch/spf248/twitter/data/demographics/data/profile_pictures/{tar_folder_dict[args.tar_type]}/{args.country_code}'
-    outfile_path = f'/scratch/spf248/twitter/data/demographics/data/profile_pictures/tars/list_files_{args.country_code}.txt.gz'
+    outfile_path = f'/scratch/spf248/twitter/data/demographics/data/profile_pictures/{tar_folder_dict[args.tar_type]}/list_files_{args.country_code}.txt.gz'
     for tar_path in Path(data_path).glob('*.tar'):
         logger.info(f'Saving file names from {tar_path}')
         tar_files = tarfile.open(tar_path)
-        for tar in tar_files.getmembers():
-            with gzip.open(outfile_path, 'wt') as f:
-                f.write(f'{tar.name}\n')
+        with gzip.open(outfile_path, 'at') as f:
+            for tar in tar_files.getmembers():
+                print(tar, file=f)
