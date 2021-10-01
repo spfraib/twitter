2cb427622e309c35b347321bc3725a775cc0f5fb
Manuel <manuel.tonneau@mailbox.org>
2021-10-01 22:16:39 +0200

    Add logs

--- code/5-demographics/get_pictures/delete_dupes_and_too_small_pics.py
+++ code/5-demographics/get_pictures/delete_dupes_and_too_small_pics.py
@@ -24,6 +24,7 @@ def get_args_from_command_line():
 
 if __name__ == '__main__':
     args = get_args_from_command_line()
+    logger.info(f'Country code: {args.country_code}')
     tar_folder_dict = {'normal': 'tars', 'resized': 'resized_tars'}
     data_path = f'/scratch/spf248/twitter/data/demographics/profile_pictures/{tar_folder_dict[args.tar_type]}/{args.country_code}'
     to_delete_dict = dict()
@@ -39,9 +40,10 @@ if __name__ == '__main__':
                 if content.size[0] < 224:
                     if not member.name in to_delete_dict[tar_path.name]:
                         to_delete_dict[tar_path.name].append(member.name)
+    count = 0
     for tar_path_name in to_delete_dict.keys():
         for member_name in to_delete_dict[tar_path_name]:
             logger.info(f'Deleting {member_name} in {tar_path_name}')
             os.system(f'tar -vf {os.path.join(data_path, tar_path_name)} --delete {member_name}')
-
-
+            count += 1
+    logger.info(f'Deleted {count} images')
