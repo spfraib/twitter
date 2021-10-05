92f85bb1c56e36ac3ba5a21b7073fbfab64d6c8e
Manuel <manuel.tonneau@mailbox.org>
2021-10-05 12:35:34 +0200

    Add code to generate user image map

---
+++ code/5-demographics/inference/generate_user_image_map.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+import tarfile
+import logging
+import argparse
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def get_args_from_command_line():
+    """Parse the command line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--country_code", type=str,
+                        default="US")
+    args = parser.parse_args()
+    return args
+
+def generate_user_image_map(tar_dir):
+    user_image_mapping = dict()
+    for tar_path in Path(tar_dir).glob('*.tar'):
+        if 'err' not in tar_path.name:
+            try:
+                tfile = tarfile.open(tar_path, 'r', ignore_zeros=True)
+                raw_paths_list = tfile.getnames()
+                for raw_path in raw_paths_list:
+                    filename = os.path.basename(raw_path)
+                    filename_no_ext = os.path.splitext(filename)[0]
+                    if filename_no_ext.isdigit():
+                        uid = filename_no_ext
+                    else:
+                        logger.info(f'Filename {filename_no_ext} (original filename: {filename}) is not a user ID.')
+                        uid = None
+                    if uid:
+                        user_image_mapping[uid] = (tar_path.name, filename)
+                    # saves <id>: (path_to_tar, file_member)
+                    # Example: '1182331536': ('../resized_tars/BR/118.tar', '1182331536.jpeg'),
+            except Exception as e:
+                logger.info(f'Exception {e} when treating {tar_path.name}')
+    return user_image_mapping
+
+if __name__ == '__main__':
+    args = get_args_from_command_line()
+    tar_dir = f'/scratch/spf248/twitter/data/demographics/profile_pictures/tars/{args.country_code}'
+    generate_user_image_map(tar_dir=tar_dir)
