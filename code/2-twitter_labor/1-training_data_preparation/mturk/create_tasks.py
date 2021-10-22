381a6722080d0c515dbd813a66d13fb5dead4bd9
Manuel <manuel.tonneau@mailbox.org>
2021-10-22 14:57:01 +0200

    Add latest changes

--- code/2-twitter_labor/1-training_data_preparation/mturk/create_tasks.py
+++ code/2-twitter_labor/1-training_data_preparation/mturk/create_tasks.py
@@ -61,7 +61,9 @@ time_to_complete = int(math.ceil((3 / 5) * ntweets))
 money_per_tweet_dict = {
     'US': 0.1,
     'BR': 0.06,
-    'MX': 0.1
+    'MX': 0.1,
+    'NG': 0.1,
+    'PK': 0.1
 }
 
 money_for_hit = int(money_per_tweet_dict[args.country_code] * ntweets)
