71371c1ca512450eabd1ea816c23d0cceffa0414
Manuel <manuel.tonneau@mailbox.org>
2022-11-11 22:25:39 +0000

    modify paths

--- code/5-demographics/get_pictures/get_filenames_from_tars.py
+++ code/5-demographics/get_pictures/get_filenames_from_tars.py
@@ -16,7 +16,7 @@ def get_args_from_command_line():
     """Parse the command line arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument("--country_code", type=str,
-                        default="US")
+                        default="NG")
     parser.add_argument("--tar_type", type=str, help='Whether to look at the tar or resized_tars', default='normal')
     args = parser.parse_args()
     return args
@@ -25,9 +25,9 @@ def get_args_from_command_line():
 if __name__ == '__main__':
     args = get_args_from_command_line()
     tar_folder_dict = {'normal': 'tars', 'resized': 'resized_tars'}
-    data_path = f'/scratch/spf248/twitter_data_collection/data/demographics/profile_pictures/{tar_folder_dict[args.tar_type]}'
-    outfile_path = f'/scratch/spf248/twitter_data_collection/data/demographics/profile_pictures/{tar_folder_dict[args.tar_type]}/list_files_all.txt.gz'
-    outfile_err_path = f'/scratch/spf248/twitter_data_collection/data/demographics/profile_pictures/{tar_folder_dict[args.tar_type]}/list_errors_all.txt.gz'
+    data_path = f'/scratch/spf248/twitter_data_collection/data/profile_pictures/{args.country_code}/{tar_folder_dict[args.tar_type]}'
+    outfile_path = f'/scratch/spf248/twitter_data_collection/data/profile_pictures/{args.country_code}/{tar_folder_dict[args.tar_type]}/list_files_all.txt.gz'
+    outfile_err_path = f'/scratch/spf248/twitter_data_collection/data/profile_pictures/{args.country_code}/{tar_folder_dict[args.tar_type]}/list_errors_all.txt.gz'
     if os.path.exists(outfile_path):
         os.remove(outfile_path)
     if os.path.exists(outfile_err_path):
